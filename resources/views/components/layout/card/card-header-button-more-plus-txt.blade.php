e7a887bb5beb81807405d63bf99d12862a885d3b
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2023-08-10 12:40:34 -0300

    sis

---
+++ resources/views/components/layout/card/card-header-button-more-plus-txt.blade.php
@@ -0,0 +1,3 @@
+<a type="button" wire:click="addTxt" class="btn btn-link btn-sm float-end" data-bs-toggle="modal" data-bs-target="#addTxtModal" title="Novo">
+    <i class="bi-filetype-txt text-success" style="font-size: 30px;"></i>
+</a>
---
+++ resources/views/components/layout/card/card-header-button-more-plus-xml.blade.php
@@ -0,0 +1,3 @@
+<a type="button" wire:click="addXml" class="btn btn-link btn-sm float-end" data-bs-toggle="modal" data-bs-target="#addXmlModal" title="Novo">
+    <i class="bi-filetype-xml text-success" style="font-size: 30px;"></i>
+</a>
