91b18fafbba4ddc7a57674da8d569d134b552af0
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2023-08-10 15:13:13 -0300

    sis

--- database/migrations/2023_08_10_150751_create_invoiceitems_table.php
+++ database/migrations/2023_08_10_150751_create_invoiceitems_table.php
@@ -13,7 +13,55 @@ return new class extends Migration
     {
         Schema::create('invoiceitems', function (Blueprint $table) {
             $table->id();
+
+            $table->unsignedBigInteger('invoice_id');
+
+            $table->boolean('equipment')->default(false);
+
+            $table->unsignedBigInteger('productgroup_id')->nullable()->default(null);
+            $table->unsignedBigInteger('invoicecsv_id')->nullable()->default(null);
+
+            $table->string('signal')->default('divide');
+            $table->float('amount', 12, 3)->default(1.000);
+
+            $table->string('identifier');
+            $table->string('code');
+            $table->string('ean')->nullable();
+            $table->string('name');
+            $table->string('ncm');
+            $table->string('cfop');
+            $table->string('cest')->nullable()->defaut(null);
+            $table->string('measure');
+
+            $table->float('quantity', 12, 3);
+            $table->float('quantity_final', 12, 3);
+            $table->float('value', 12, 3);
+            $table->float('value_final', 12, 3);
+            $table->float('value_total', 12, 3);
+            $table->float('value_total_final', 12, 3);
+
+            $table->float('ipi', 12, 3);
+            $table->float('ipi_final', 12, 3);
+            $table->float('ipi_aliquot', 12, 3);
+            $table->float('ipi_aliquot_final', 12, 3);
+
+            $table->float('margin', 12, 2);
+            $table->float('shipping', 12, 2);
+
+            $table->float('discount', 12, 2);
+            $table->float('addition', 12, 2);
+
+            $table->boolean('updated')->default(false);
+
+            $table->float('index', 12, 2)->nullable()->default(null);
+
+            $table->float('price', 12, 2)->nullable()->default(null);
+            $table->float('card', 12, 2)->nullable()->default(null);
+            $table->float('retail', 12, 2)->nullable()->default(null);
+
             $table->timestamps();
+
+            $table->foreign('invoice_id')->references('id')->on('invoices');
         });
     }
 
