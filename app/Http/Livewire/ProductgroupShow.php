d1303fdf0ea2f06b7fcd0af24e2cb31ddf87f68f
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2023-08-10 12:17:50 -0300

    sis

---
+++ app/Http/Livewire/ProductgroupShow.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class ProductgroupShow extends Component
+{
+    public function render()
+    {
+        return view('livewire.productgroup-show');
+    }
+}
