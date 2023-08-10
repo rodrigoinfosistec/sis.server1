cda1ac8337da4f0d33929b3a9433bd96b0245d02
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2023-08-10 13:04:21 -0300

    sis

--- app/Models/Report.php
+++ app/Models/Report.php
@@ -135,4 +135,182 @@ class Report extends Model
 
         return true;
     }
+
+    /**
+     * Company Generate
+     * @var array $data
+     * 
+     * @return bool true
+     */
+    public static function companyGenerate(array $data) : bool {
+        // Gera o arquivo PDF.
+        $pdf = PDF::loadView('components.' . $data['config']['name'] . '.pdf', [
+            'user'  => auth()->user()->name,
+            'title' => $data['config']['title'],
+            'date'  => date('d/m/Y H:i:s'),
+            'list'  => $list = Company::where([
+                            [$data['filter'], 'like', '%'. $data['search'] . '%'],
+                        ])->orderBy('name', 'ASC')->get(), 
+        ])->set_option('isPhpEnabled', true)->setPaper('A4', 'portrait');
+
+        // Salva o arquivo PDF.
+        File::makeDirectory($data['path'], $mode = 0777, true, true);
+        $pdf->save($data['path'] . $data['file_name']);
+
+        // Registra os dados do arquivo PDF.
+        Report::create([
+            'user_id' => auth()->user()->id,
+            'folder'  => $data['config']['name'],
+            'file'    => $data['file_name']
+        ]);
+
+        return true;
+    }
+
+    /**
+     * Company Xml
+     * @var array $data
+     * 
+     * @return <object, null> $xml 
+     */
+    public static function xmlCompany(array $data){
+        // Salva o arquivo xml.
+        $file_name = $data['config']['name'] . '_' . auth()->user()->id . '_' . Str::random(20) . '.xml';
+        $path      = public_path('/storage/xml/' . $data['config']['name'] . '/');
+        File::makeDirectory($path, $mode = 0777, true, true);
+        $data['validatedData']['xml']->storeAs('public/xml/' . $data['config']['name'] . '/', $file_name);
+
+        // Instancia o objeto Xml.
+        $xmlFile    = file_get_contents($path . $file_name);
+        @$xmlObject = simplexml_load_string($xmlFile);
+
+        // verifica se é um xml.
+        if($xmlObject):
+            // "Renomeia" o arquivo com a chave do xml.
+            $data['validatedData']['xml']->storeAs('public/xml/' . $data['config']['name'] . '/', $xmlObject->protNFe->infProt->chNFe . '.xml');
+            unlink($path . $file_name);
+
+            // Areibui à variável.
+            $xml = $xmlObject;
+        else:
+            // Exclui o arquivo.
+            unlink($path . $file_name);
+
+            // Areibui à variável.
+            $xml = null;
+        endif;
+
+        return  $xml;
+    }
+
+    /**
+     * Company Txt
+     * @var array $data
+     * 
+     * @return <object, null> $txt
+     */
+    public static function txtCompany(array $data){
+        // Salva o arquivo txt.
+        $file_name = $data['config']['name'] . '_' . auth()->user()->id . '_' . Str::random(20) . '.txt';
+        $path = public_path('/storage/txt/' . $data['config']['name'] . '/');
+        File::makeDirectory($path, $mode = 0777, true, true);
+        $data['validatedData']['txt']->storeAs('public/txt/' . $data['config']['name'] . '/', $file_name);
+
+        // Instancia dados do txt.
+        $data = file($path . $file_name); 
+
+        // Verifica se é um txt de empregador.
+        if($data[0][0] == '2' && $data[0][3] == ']'):
+            // Separa dados.
+            $l = explode(']', $data[0]);
+
+            // Monta o array.
+            $txtArray = [
+                'cnpj'     => $l[1],
+                'name'     => $l[3],
+                'nickname' => '',
+                'path'     => $path . $file_name,
+            ];
+
+            // Atribui à variável.
+            $txt = $txtArray;
+        else:
+            // Exclui o arquivo.
+            unlink($path . $file_name);
+
+            // Atribui à variável.
+            $txt = null;
+        endif;
+
+        return  $txt;
+    }
+
+    /**
+     * Provider Generate
+     * @var array $data
+     * 
+     * @return bool true
+     */
+    public static function providerGenerate(array $data) : bool {
+        // Gera o arquivo PDF.
+        $pdf = PDF::loadView('components.' . $data['config']['name'] . '.pdf', [
+            'user'  => auth()->user()->name,
+            'title' => $data['config']['title'],
+            'date'  => date('d/m/Y H:i:s'),
+            'list'  => $list = Provider::where([
+                            [$data['filter'], 'like', '%'. $data['search'] . '%'],
+                        ])->orderBy('name', 'ASC')->get(), 
+        ])->set_option('isPhpEnabled', true)->setPaper('A4', 'portrait');
+
+        // Salva o arquivo PDF.
+        File::makeDirectory($data['path'], $mode = 0777, true, true);
+        $pdf->save($data['path'] . $data['file_name']);
+
+        // Registra os dados do arquivo PDF.
+        Report::create([
+            'user_id' => auth()->user()->id,
+            'folder'  => $data['config']['name'],
+            'file'    => $data['file_name']
+        ]);
+
+        return true;
+    }
+
+    /**
+     * Provider Xml
+     * @var array $data
+     * 
+     * @return <object, null> $xml 
+     */
+    public static function xmlProvider(array $data){
+        // Salva o arquivo xml.
+        $file_name = $data['config']['name'] . '_' . auth()->user()->id . '_' . Str::random(20) . '.xml';
+        $path      = public_path('/storage/xml/' . $data['config']['name'] . '/');
+        File::makeDirectory($path, $mode = 0777, true, true);
+        $data['validatedData']['xml']->storeAs('public/xml/' . $data['config']['name'] . '/', $file_name);
+
+        // Instancia o objeto Xml.
+        $xmlFile    = file_get_contents($path . $file_name);
+        @$xmlObject = simplexml_load_string($xmlFile);
+
+        // verifica se é um xml.
+        if($xmlObject):
+            // "Renomeia" o arquivo com a chave do xml.
+            $data['validatedData']['xml']->storeAs('public/xml/' . $data['config']['name'] . '/', $xmlObject->protNFe->infProt->chNFe . '.xml');
+
+            // Exclui o arquivo.
+            unlink($path . $file_name);
+
+            // Atribui à variável.
+            $xml = $xmlObject;
+        else:
+            // Exclui o arquivo.
+            unlink($path . $file_name);
+
+            // Atribui à variável.
+            $xml = null;
+        endif;
+
+        return  $xml;
+    }
 }
