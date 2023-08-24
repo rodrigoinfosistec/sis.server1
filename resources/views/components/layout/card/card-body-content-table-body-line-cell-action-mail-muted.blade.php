8a43c28a6603c55d0edf34fc27fcf60c0f893960
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2023-08-24 14:39:29 -0300

    sis

---
+++ resources/views/components/layout/card/card-body-content-table-body-line-cell-action-mail-muted.blade.php
@@ -0,0 +1,3 @@
+<a type="button" class="btn btn-link btn-sm disabled" style="padding: 0px 5px 0px 5px;" title="Editar">
+    <i class="bi-envelope-fill text-muted" style="font-size: 20px;"></i>
+</a>
---
+++ resources/views/components/layout/card/card-body-content-table-body-line-cell-action-zip-muted.blade.php
@@ -0,0 +1,3 @@
+<a type="button" class="btn btn-link btn-sm disabled" style="padding: 0px 5px 0px 5px;" title="Editar">
+    <i class="bi-cloud-download-fill text-muted" style="font-size: 20px;"></i>
+</a>
