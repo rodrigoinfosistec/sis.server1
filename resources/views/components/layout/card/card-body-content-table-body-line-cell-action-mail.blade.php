90d5ca0a6d9ba643dd014cf4223ccd2971cf17eb
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2023-08-24 14:45:25 -0300

    sis

--- resources/views/components/layout/card/card-body-content-table-body-line-cell-action-mail.blade.php
+++ resources/views/components/layout/card/card-body-content-table-body-line-cell-action-mail.blade.php
@@ -1,3 +1,3 @@
-<div>
-    <!-- Be present above all else. - Naval Ravikant -->
-</div>
+<a type="button" wire:click="editItemPrice({{ $id }})" class="btn btn-link btn-sm" style="padding: 0px 5px 0px 5px;" data-bs-toggle="modal" data-bs-target="#editItemPriceModal" title="Enviar e-mail">
+    <i class="bi-envelope-fill text-primary" style="font-size: 20px;"></i>
+</a>
