b76b2aaa564e0fa5bac2d3c6658d8b45b12e6e5b
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2023-08-24 17:24:02 -0300

    sis

--- resources/views/components/layout/card/card-body-content-table-body-line-cell-action-print-muted.blade.php
+++ resources/views/components/layout/card/card-body-content-table-body-line-cell-action-print-muted.blade.php
@@ -1,3 +1,3 @@
-<div>
-    <!-- The best way to take care of the future is to take care of the present moment. - Thich Nhat Hanh -->
-</div>
+<a type="button" class="btn btn-link btn-sm disabled" style="padding: 0px 5px 0px 5px;" title="Enviar e-mail">
+    <i class="bi-printer-fill text-muted" style="font-size: 20px;"></i>
+</a>
