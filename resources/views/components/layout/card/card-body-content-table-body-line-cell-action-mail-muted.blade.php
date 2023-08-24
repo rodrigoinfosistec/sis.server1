aadeb3db3aec1fa37bca16257aeee115a746dd35
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2023-08-24 14:52:20 -0300

    sis

--- resources/views/components/layout/card/card-body-content-table-body-line-cell-action-mail-muted.blade.php
+++ resources/views/components/layout/card/card-body-content-table-body-line-cell-action-mail-muted.blade.php
@@ -1,3 +1,3 @@
 <a type="button" class="btn btn-link btn-sm disabled" style="padding: 0px 5px 0px 5px;" title="Enviar e-mail">
-    <i class="bi-cloud-envelop-fill text-muted" style="font-size: 20px;"></i>
+    <i class="bi-envelope-fill text-muted" style="font-size: 20px;"></i>
 </a>
--- resources/views/components/layout/card/card-body-content-table-body-line-cell-action-zip-muted.blade.php
+++ resources/views/components/layout/card/card-body-content-table-body-line-cell-action-zip-muted.blade.php
@@ -1,3 +1,3 @@
-<a type="button" class="btn btn-link btn-sm disabled" style="padding: 0px 5px 0px 5px;" title="Editar">
+<a type="button" class="btn btn-link btn-sm disabled" style="padding: 0px 5px 0px 5px;" title="Baixar Preços ZIP">
     <i class="bi-cloud-download-fill text-muted" style="font-size: 20px;"></i>
 </a>
