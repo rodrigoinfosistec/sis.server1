9afb7cadef4552e2ad0b8291f262ff538870a8e6
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2023-09-04 11:38:20 -0300

    sis

--- app/Models/Holiday.php
+++ app/Models/Holiday.php
@@ -88,5 +88,182 @@ class Holiday extends Model
 
         return true;
     }
+    
+    /**
+     * Valida exclusão.
+     * @var array $data
+     * 
+     * @return bool true
+     */
+    public static function validateErase(array $data) : bool {
+        $message = null;
 
+        // ...
+
+        // Desvio.
+        if(!empty($message)):
+            session()->flash('message', $message );
+            session()->flash('color', 'danger');
+
+            return false;
+        endif;
+
+        return true;
+    }
+
+    /**
+     * Executa dependências de exclusão.
+     * @var array $data
+     * 
+     * @return bool true
+     */
+    public static function dependencyErase(array $data) : bool {
+        // ...
+
+        return true;
+    }
+
+    /**
+     * Exclui.
+     * @var array $data
+     * 
+     * @return bool true
+     */
+    public static function erase(array $data) : bool {
+        // Exclui.
+        Holiday::find($data['validatedData']['holiday_id'])->delete();
+
+        // Auditoria.
+        Audit::holidayErase($data);
+
+        // Mensagem.
+        $message = $data['config']['title'] . ' ' .  $data['validatedData']['name'] . ' excluído com sucesso.';
+        session()->flash('message', $message);
+        session()->flash('color', 'success');
+
+        return true;
+    }
+
+    /**
+     * Valida geração de relatório.
+     * @var array $data
+     * 
+     * @return bool true
+     */
+    public static function validateGenerate(array $data) : bool {
+        $message = null;
+
+        // verifica se existe algum item retornado na pesquisa.
+        if($list = Holiday::where([
+            [$data['filter'], 'like', '%'. $data['search'] . '%'],
+        ])->doesntExist()):
+
+            $message = 'Nenhum ítem selecionado.';
+        endif;
+
+        // Desvio.
+        if(!empty($message)):
+            session()->flash('message', $message );
+            session()->flash('color', 'danger');
+
+            return false;
+        endif;
+
+        return true;
+    }
+
+    /**
+     * Gera relatório.
+     * @var array $data
+     * 
+     * @return bool true
+     */
+    public static function generate(array $data) : bool {
+        // Estende $data.
+        $data['path']      = public_path('/storage/pdf/' . $data['config']['name'] . '/');
+        $data['file_name'] = $data['config']['name'] . '_' . auth()->user()->id . '_' . Str::random(20) . '.pdf';
+
+        // Gera o PDF.
+        Report::holidayGenerate($data);
+
+        // Auditoria.
+        Audit::holidayGenerate($data);
+
+        // Mensagem.
+        $message = 'Relatório PDF gerado com sucesso.';
+        session()->flash('message', $message);
+        session()->flash('color', 'success');
+
+        return true;
+    }
+
+    /**
+     * Executa dependências de geração de relatório.
+     * @var array $data
+     * 
+     * @return bool true
+     */
+    public static function dependencyGenerate(array $data) : bool {
+        //...
+
+        return true;
+    }
+
+    /**
+     * Valida envio de e-mail.
+     * @var array $data
+     * 
+     * @return bool true
+     */
+    public static function validateMail(array $data) : bool {
+        $message = null;
+
+        // Verifica conexão com a internet.
+        if(checkdnsrr('google.com') < 1):
+            $message = 'Sem conexão com a internet..';
+        endif;
+
+        // Desvio.
+        if(!empty($message)):
+            session()->flash('message', $message );
+            session()->flash('color', 'danger');
+
+            return false;
+        endif;
+
+        return true;
+    }
+
+    /**
+     * Envia e-mail.
+     * @var array $data
+     * 
+     * @return bool true
+     */
+    public static function mail(array $data) : bool {
+        // Envia e-mail.
+        Email::holidayMail($data);
+
+        // Auditoria.
+        Audit::holidayMail($data);
+
+        // Mensagem.
+        $message = 'E-mail para ' . $data['validatedData']['mail'] . ' enviado com sucesso.';
+        session()->flash('message', $message);
+        session()->flash('color', 'success');
+
+        return true;
+    }
+
+    /**
+     * Executa dependências de envio de e-mail.
+     * @var array $data
+     * 
+     * @return bool true
+     */
+    public static function dependencyMail(array $data) : bool {
+        //...
+
+        return true;
+    }
 }
