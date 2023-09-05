8e62eda7b9268e55271b248c274dd53bb576a6c3
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2023-09-05 12:47:56 -0300

    sis

--- app/Http/Controllers/EmployeeabsenceController.php
+++ app/Http/Controllers/EmployeeabsenceController.php
@@ -4,7 +4,33 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Page;
+
 class EmployeeabsenceController extends Controller
 {
-    //
+    public $pageName;
+
+    public function index(){
+        /**
+         * Nome da página - Configurável e único.
+         * @var string $this->pageName.
+         */
+        $this->pageName = 'employeeabsence';
+
+        /**
+         * Expulsa Usuário sem autorização de acesso à Página.
+         */
+        if (!Page::userAuthorized($this->pageName)) return redirect()->to('/home');
+
+        /**
+         * View.
+         */
+        return view('index', [
+            'config' => [
+                'name'  => $this->pageName,
+                'title' => Page::getTitleByName($this->pageName), 
+                'icon'  => Page::getIconByName($this->pageName), 
+            ],
+        ]);
+    }
 }
