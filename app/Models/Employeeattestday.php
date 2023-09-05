26e5f4e89cf1dc8ce2103649067368f812e888cc
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2023-09-05 16:03:56 -0300

    sis

--- app/Models/Employeeattestday.php
+++ app/Models/Employeeattestday.php
@@ -2,10 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Employeeattestday extends Model
 {
-    use HasFactory;
+    
 }
