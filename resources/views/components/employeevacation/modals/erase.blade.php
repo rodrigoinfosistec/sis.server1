a030702af6e9c8b867be55052a69e66307de8e8c
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2023-09-05 14:16:30 -0300

    sis

--- resources/views/components/employeevacation/modals/erase.blade.php
+++ resources/views/components/employeevacation/modals/erase.blade.php
@@ -1,3 +1,70 @@
-<div>
-    <!-- It is not the man who has too little, but the man who craves more, that is poor. - Seneca -->
-</div>
+<x-layout.modal.modal-erase modal="erase" method="exclude" form="true" size="">
+    <x-layout.modal.modal-erase-header icon="bi-trash3" modal="erase">
+        {{ $config['title'] }}
+        
+        <x-slot:identifier>
+            {{-- ... --}}
+        </x-slot>
+    </x-layout.modal.modal-erase-header>
+
+    <x-layout.modal.modal-erase-body>
+        <x-slot:question>
+            Deseja realmente excluir?
+        </x-slot>
+
+        <x-slot:thead>
+            {{-- ... --}}
+        </x-slot>
+
+{{-- conteúdo --}}
+<x-layout.modal.modal-erase-body-line>
+    <x-layout.modal.modal-erase-body-line-title>
+        ID
+    </x-layout.modal.modal-erase-body-line-title>
+    <x-layout.modal.modal-erase-body-line-content>
+        {{ $employeevacation_id }}
+    </x-layout.modal.modal-erase-body-line-content>
+</x-layout.modal.modal-erase-body-line>
+
+<x-layout.modal.modal-erase-body-line>
+    <x-layout.modal.modal-erase-body-line-title>
+        FUNCIONÁRIO
+    </x-layout.modal.modal-erase-body-line-title>
+    <x-layout.modal.modal-erase-body-line-content>
+        {{ $employee_name }}
+    </x-layout.modal.modal-erase-body-line-content>
+</x-layout.modal.modal-erase-body-line>
+
+<x-layout.modal.modal-erase-body-line>
+    <x-layout.modal.modal-erase-body-line-title>
+        INÍCIO
+    </x-layout.modal.modal-erase-body-line-title>
+    <x-layout.modal.modal-erase-body-line-content>
+        {{ $date_start }}
+    </x-layout.modal.modal-erase-body-line-content>
+</x-layout.modal.modal-erase-body-line>
+
+<x-layout.modal.modal-erase-body-line>
+    <x-layout.modal.modal-erase-body-line-title>
+        FINAL
+    </x-layout.modal.modal-erase-body-line-title>
+    <x-layout.modal.modal-erase-body-line-content>
+        {{ $date_end }}
+    </x-layout.modal.modal-erase-body-line-content>
+</x-layout.modal.modal-erase-body-line>
+
+<x-layout.modal.modal-erase-body-line>
+    <x-layout.modal.modal-erase-body-line-title>
+        CADASTRO
+    </x-layout.modal.modal-erase-body-line-title>
+    <x-layout.modal.modal-erase-body-line-content>
+        {{ $created }}
+    </x-layout.modal.modal-erase-body-line-content>
+</x-layout.modal.modal-erase-body-line>
+{{-- conteúdo --}}
+
+    </x-layout.modal.modal-erase-body>
+
+    <x-layout.modal.modal-erase-footer/>
+
+</x-layout.modal.modal-erase>
