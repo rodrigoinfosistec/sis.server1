2a15d39bc4566ffc694163c56293876402cb3d7a
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2023-09-06 16:11:42 -0300

    sis

---
+++ app/Http/Livewire/EmployeeeasyShow.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class EmployeeeasyShow extends Component
+{
+    public function render()
+    {
+        return view('livewire.employeeeasy-show');
+    }
+}
