00106b6d031eddc00bd3e1e8bb9a33da9537316c
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2023-09-06 09:56:14 -0300

    sis

---
+++ database/migrations/2023_09_06_094549_add_company_to_users.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('company_id')
+                ->after('id')
+                ->nullable()->default(null);
+
+            $table->string('company_name')
+                ->after('company_id')
+                ->nullable()->default(null);
+
+            $table->foreign('company_id')->references('id')->on('companies');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
