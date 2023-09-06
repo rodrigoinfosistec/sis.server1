4e410f9763e7662c9d8b6d86c5ea6d7159100e02
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2023-09-06 16:44:20 -0300

    sis

--- resources/views/components/employeeeasy/modals/detail.blade.php
+++ resources/views/components/employeeeasy/modals/detail.blade.php
@@ -1,3 +1,64 @@
-<div>
-    <!-- Simplicity is an acquired taste. - Katharine Gerould -->
-</div>
+<x-layout.modal.modal-detail modal="detail" size="">
+    <x-layout.modal.modal-detail-header icon="bi-eye" modal="detail">
+        {{ $config['title'] }}
+
+        <x-slot:identifier>
+            {{-- ... --}}
+        </x-slot>
+    </x-layout.modal.modal-detail-header>
+
+    <x-layout.modal.modal-detail-body>
+
+{{-- conteúdo --}}
+<x-layout.modal.modal-detail-body-line>
+    <x-layout.modal.modal-detail-body-line-title>
+        ID
+    </x-layout.modal.modal-detail-body-line-title>
+    <x-layout.modal.modal-detail-body-line-content>
+        {{ $employeeeasy_id }}
+    </x-layout.modal.modal-detail-body-line-content>
+</x-layout.modal.modal-detail-body-line>
+
+<x-layout.modal.modal-detail-body-line>
+    <x-layout.modal.modal-detail-body-line-title>
+        FUNCIONÁRIO
+    </x-layout.modal.modal-detail-body-line-title>
+    <x-layout.modal.modal-detail-body-line-content>
+        {{ $employee_name }}
+    </x-layout.modal.modal-detail-body-line-content>
+</x-layout.modal.modal-detail-body-line>
+
+<x-layout.modal.modal-detail-body-line>
+    <x-layout.modal.modal-detail-body-line-title>
+        DATA
+    </x-layout.modal.modal-detail-body-line-title>
+    <x-layout.modal.modal-detail-body-line-content>
+        {{ $date }}
+    </x-layout.modal.modal-detail-body-line-content>
+</x-layout.modal.modal-detail-body-line>
+
+<x-layout.modal.modal-detail-body-line>
+    <x-layout.modal.modal-detail-body-line-title>
+        DESCONTA DO BANCO DE HORAS?
+    </x-layout.modal.modal-detail-body-line-title>
+    <x-layout.modal.modal-detail-body-line-content>
+        @if($discount)
+            <span class="text-success">SIM</span>
+        @else
+            <span class="text-danger">Inativo</span>
+        @endif
+    </x-layout.modal.modal-detail-body-line-content>
+</x-layout.modal.modal-detail-body-line>
+
+<x-layout.modal.modal-detail-body-line>
+    <x-layout.modal.modal-detail-body-line-title>
+        CADASTRO
+    </x-layout.modal.modal-detail-body-line-title>
+    <x-layout.modal.modal-detail-body-line-content>
+        {{ $created }}
+    </x-layout.modal.modal-detail-body-line-content>
+</x-layout.modal.modal-detail-body-line>
+{{-- conteúdo --}}
+
+    </x-layout.modal.modal-detail-body>
+</x-layout.modal.modal-detail>
