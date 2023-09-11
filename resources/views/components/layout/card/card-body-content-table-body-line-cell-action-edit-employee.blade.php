7075f568df015aaaff2b31f2592bbed905542146
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2023-09-11 15:49:43 -0300

    sis

--- resources/views/components/layout/card/card-body-content-table-body-line-cell-action-edit-employee.blade.php
+++ resources/views/components/layout/card/card-body-content-table-body-line-cell-action-edit-employee.blade.php
@@ -1,3 +1,3 @@
-<a type="button" wire:click="editEmployee({{ $id }})" class="btn btn-link btn-sm" style="padding: 0px 2px 0px 2px;" data-bs-toggle="modal" data-bs-target="#editEmployeeModal" title="Editar Horas do Funcionário">
+<a type="button" wire:click="editEmployee({{ $id }})" class="btn btn-link btn-sm" style="padding: 0px 2px 0px 2px;" data-bs-toggle="modal" data-bs-target="#editEmployeeModal" title="Editar Horas">
     <i class="bi-fingerprint text-primary" style="font-size: 17px;"></i>
 </a>
