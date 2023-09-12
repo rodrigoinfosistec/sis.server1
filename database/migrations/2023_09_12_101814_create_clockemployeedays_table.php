3923a30bc84934746831a06aafa17233b1f43e03
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2023-09-12 10:24:37 -0300

    sis

--- database/migrations/2023_09_12_101814_create_clockemployeedays_table.php
+++ database/migrations/2023_09_12_101814_create_clockemployeedays_table.php
@@ -13,7 +13,28 @@ return new class extends Migration
     {
         Schema::create('clockemployeedays', function (Blueprint $table) {
             $table->id();
+
+            $table->unsignedBigInteger('clockemployee_id');
+
+            $table->string('input')->nullable()->default(null);
+            $table->string('break-start')->nullable()->default(null);
+            $table->string('break_end')->nullable()->default(null);
+            $table->string('output')->nullable()->default(null);
+
+            $table->string('journey_start')->nullable()->default(null);
+            $table->string('journey_end')->nullable()->default(null);
+            $table->string('journey_break')->nullable()->default(null);
+
+            $table->string('delay_total')->nullable()->default(null);
+            $table->string('delay_total')->nullable()->default(null);
+            $table->string('extra_total')->nullable()->default(null);
+            $table->string('balance_total')->nullable()->default(null);
+
+            $table->boolean('authorized')->default(false);
+
             $table->timestamps();
+
+            $table->foreign('clockemployee_id')->references('id')->on('clockemployees');
         });
     }
 
