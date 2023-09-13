6f4d549b5f7fc78112af6adf164acabfc266c69c
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2023-09-13 15:28:52 -0300

    sis

--- resources/views/components/clock/modals/edit-clock-employee.blade.php
+++ resources/views/components/clock/modals/edit-clock-employee.blade.php
@@ -136,6 +136,30 @@
         </div>
     </td>
 
+{{-- FÉRIAS --}}
+@elseif(App\Models\Employeevacationday::where(['employee_id' => $clockemployee_employee_id, 'date' => $date])->orderBy('id', 'DESC')->first())
+    <td colspan="100%" class="align-middle" style="line-height: 1; padding: 0; background-color: #e9e9e9;">
+        <div class="text-muted fw-bold text-center" style="font-size: 9pt; margin: 10px 0 10px 0;">
+            FÉRIAS ({{ date_format(date_create($date), 'd/m/y') }})
+        </div>
+    </td>
+
+{{-- ATESTADO --}}
+@elseif(App\Models\Employeeattestday::where(['employee_id' => $clockemployee_employee_id, 'date' => $date])->orderBy('id', 'DESC')->first())
+    <td colspan="100%" class="align-middle" style="line-height: 1; padding: 0; background-color: #e9e9e9;">
+        <div class="text-muted fw-bold text-center" style="font-size: 9pt; margin: 10px 0 10px 0;">
+            ATESTADO ({{ date_format(date_create($date), 'd/m/y') }})
+        </div>
+    </td>
+
+{{-- FALTA --}}
+@elseif(App\Models\Employeeabsenceday::where(['employee_id' => $clockemployee_employee_id, 'date' => $date])->orderBy('id', 'DESC')->first())
+    <td colspan="100%" class="align-middle" style="line-height: 1; padding: 0; background-color: #e9e9e9;">
+        <div class="text-muted fw-bold text-center" style="font-size: 9pt; margin: 10px 0 10px 0;">
+            ATESTADO ({{ date_format(date_create($date), 'd/m/y') }})
+        </div>
+    </td>
+
 {{-- FOLGA --}}
 @elseif(App\Models\Employeeeasy::where(['employee_id' => $clockemployee_employee_id,'date' => $date])->orderBy('id', 'DESC')->first())
     <td colspan="100%" class="align-middle" style="line-height: 1; padding: 0; background-color: #e9e9e9;">
