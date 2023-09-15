c9165775df627e439d2810ad573e06d91baf62b4
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2023-09-15 11:48:04 -0300

    sis

--- resources/views/components/layout/card/card-body-content-table-body-line-cell-action-add-holiday-muted.blade.php
+++ resources/views/components/layout/card/card-body-content-table-body-line-cell-action-add-holiday-muted.blade.php
@@ -1,3 +1,3 @@
 <a type="button" wire:click="addHoliday({{ $id }})" class="btn btn-link btn-sm disabled" style="padding: 0px 5px 0px 5px;" data-bs-toggle="modal" data-bs-target="#addHolidayModal" title="Novo Feriado">
-    <i class="bi-tree-fill text-primary" style="font-size: 20px;"></i>
+    <i class="bi-tree-fill text-success" style="font-size: 20px;"></i>
 </a>
