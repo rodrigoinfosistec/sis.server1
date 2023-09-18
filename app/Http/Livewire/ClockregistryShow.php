1b5e34e78b22848fbbabc30b2a1a0914ef1b378a
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2023-09-18 12:31:06 -0300

    sis

---
+++ app/Http/Livewire/ClockregistryShow.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class ClockregistryShow extends Component
+{
+    public function render()
+    {
+        return view('livewire.clockregistry-show');
+    }
+}
---
+++ resources/views/components/clockregistry/script.blade.php
@@ -0,0 +1,8 @@
+<script>
+    window.addEventListener('close-modal', event => {
+        $('#generateModal').modal('hide');
+        $('#mailModal').modal('hide');
+
+        $('#addEasyModal').modal('hide');
+    })
+</script>
