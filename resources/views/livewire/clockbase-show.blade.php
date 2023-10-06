0ba4699bec58a6d873db588b0263af2d2eee40e2
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2023-10-06 10:32:52 -0300

    sis

--- resources/views/livewire/clockbase-show.blade.php
+++ resources/views/livewire/clockbase-show.blade.php
@@ -39,7 +39,23 @@
 
                     @if($existsReport)
                         <x-layout.card.card-header-button-action-print>
-                            <x-layout.card.card-header-button-action-print-for :config="$config" :reports="$reports"/>
+                            @foreach($reports as $key => $report)
+                                <li>
+                                    <a class="dropdown-item" href="{{ asset('storage/pdf/' . $config['name'] . '/' . $report->file) }}" target="_blank">
+                                        <span class="text-uppercase text-dark fw-bold" style="font-size: 8pt;">
+                                            <i class="bi-file-pdf text-danger" style="font-size: 15px;"></i>
+
+                                            <span class="fst-italic">
+                                                {{ str_pad($report->id , Str::length($reports->count()), '0', STR_PAD_LEFT); }}
+                                            </span>
+
+                                            {{ $report->user->name }}
+
+                                            {{ date_format($report->created_at, "d/m/Y H:i:s") }}
+                                        </span>
+                                    </a>
+                                </li>
+                            @endforeach
                         </x-layout.card.card-header-button-print>
                     @else
                         <x-layout.card.card-header-button-action-print-muted/>
