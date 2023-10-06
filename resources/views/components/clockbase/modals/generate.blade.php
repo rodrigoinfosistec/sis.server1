dca331f6b901d73ee13403af2de5c21d8b359335
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2023-10-06 09:48:12 -0300

    sis

--- resources/views/components/clockbase/modals/generate.blade.php
+++ resources/views/components/clockbase/modals/generate.blade.php
@@ -1,3 +1,9 @@
-<div>
-    <!-- Nothing in life is to be feared, it is only to be understood. Now is the time to understand more, so that we may fear less. - Marie Curie -->
-</div>
+<x-layout.modal.modal-generate size="">
+    <x-layout.modal.modal-generate-header>
+        {{ $config['title'] }}
+    </x-layout.modal.modal-generate-header>
+
+    <x-layout.modal.modal-generate-body :filter="$filter" :search="$search" :count="$list->total()"/>
+
+    <x-layout.modal.modal-generate-footer/>
+</x-layout.modal.modal-generate>
