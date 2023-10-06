096d567d395f10e86605e2cc1f69777c6c8fd003
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2023-10-06 09:46:23 -0300

    sis

--- resources/views/components/clockbase/pdf.blade.php
+++ resources/views/components/clockbase/pdf.blade.php
@@ -1,3 +1,67 @@
-<div>
-    <!-- Do what you can, with what you have, where you are. - Theodore Roosevelt -->
-</div>
+<x-layout.pdf.pdf>
+    @section('browser', $title)
+
+    <x-layout.pdf.pdf-header :title="$title"/>
+
+    <x-layout.pdf.pdf-signature :user="$user" :date="$date"/>
+
+    <x-layout.pdf.pdf-table>
+        <x-layout.pdf.pdf-table-header>
+
+{{-- conteúdo título --}}
+<x-layout.pdf.pdf-table-header-column>
+    #
+</x-layout.pdf.pdf-table-header-column>
+
+<x-layout.pdf.pdf-table-header-column>
+    PIS
+</x-layout.pdf.pdf-table-header-column>
+
+<x-layout.pdf.pdf-table-header-column>
+    NOME
+</x-layout.pdf.pdf-table-header-column>
+
+<x-layout.pdf.pdf-table-header-column>
+    SALDO
+</x-layout.pdf.pdf-table-header-column>
+{{-- conteúdo título --}}
+
+        </x-layout.pdf.pdf-table-header>
+
+        <x-layout.pdf.pdf-table-body>
+            @foreach($list as $item)
+                <x-layout.pdf.pdf-table-body-line>
+
+{{-- conteúdo --}}
+{{-- # --}}
+<x-layout.pdf.pdf-table-body-line-cell>
+    {{ str_pad($loop->iteration , Str::length($list->count()), '0', STR_PAD_LEFT) }}
+</x-layout.pdf.pdf-table-body-line-cell>
+
+{{-- PIS --}}
+<x-layout.pdf.pdf-table-body-line-cell>
+    {{ $item->pis }}
+</x-layout.pdf.pdf-table-body-line-cell>
+
+{{-- NOME --}}
+<x-layout.pdf.pdf-table-body-line-cell>
+    {{ $item->name }}
+</x-layout.pdf.pdf-table-body-line-cell>
+
+{{-- SALDO --}}
+<x-layout.pdf.pdf-table-body-line-cell>
+    <div style="font-size: 10pt;">
+        @if($item->datatime > 0) <span class="fw-bold">
+            @elseif($item->datatime < 0) <span class="fw-normal text-muted"> @endif
+            {{ App\Models\Clock::minutsToTimeSignal((int)$item->datatime) }}
+        </span>
+    </div>
+</x-layout.pdf.pdf-table-body-line-cell>
+
+{{-- conteúdo --}}
+
+                </x-layout.pdf.pdf-table-body-line>
+            @endforeach
+        </x-layout.pdf.pdf-table-body>
+    </x-layout.pdf.pdf-table>
+</x-app.pdf.layout>
