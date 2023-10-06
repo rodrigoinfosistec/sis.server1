28e612c59ff7a1b9f5450a2296a64b74f994500c
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2023-10-06 09:22:06 -0300

    sis

--- app/Http/Livewire/ClockShow.php
+++ app/Http/Livewire/ClockShow.php
@@ -614,6 +614,7 @@ class ClockShow extends Component
             $this->closeModal();
             $this->dispatchBrowserEvent('close-modal');
         }
+
     /**
      * generate()
      *  sire()
