1f5adaea0e254622bfca1e4d00d6ea6698c8e58c
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2023-10-27 15:30:44 -0300

    sis

--- app/Http/Livewire/EmployeeeventShow.php
+++ app/Http/Livewire/EmployeeeventShow.php
@@ -144,153 +144,18 @@ class EmployeeeventShow extends Component
                 'time' => ['required'],
             ]);
 
-            // Estende $validatedData.
-            $j = explode(':', Employee::find($validatedData['employee_id'])->journey);
-            $validatedData['journey']                    = ($j[0] * 60) + $j[1];
-            $this->discount ? $validatedData['discount'] = true : $validatedData['discount'] = false;
-
             // Define $data.
             $data['config']        = $this->config;
             $data['validatedData'] = $validatedData;
 
             // Valida cadastro.
-            $valid = Employeeeasy::validateAdd($data);
+            $valid = Employeeevent::validateAdd($data);
 
             // Cadastra.
-            if ($valid) Employeeeasy::add($data);
+            if ($valid) Employeeevent::add($data);
 
             // Executa dependências.
-            if ($valid) Employeeeasy::dependencyAdd($data);
-
-            // Fecha modal.
-            $this->closeModal();
-            $this->dispatchBrowserEvent('close-modal');
-        }
-
-    /** 
-     * detail()
-     */
-    public function detail(int $employeeeasy_id)
-    {
-        // Férias.
-        $employeeeasy = Employeeeasy::find($employeeeasy_id);
-
-        // Inicializa propriedades dinâmicas.
-        $this->employeeeasy_id = $employeeeasy->id;
-        $this->employee_id     = $employeeeasy->employee_id;
-        $this->employee_name   = $employeeeasy->employee_name;
-        $this->date            = General::decodedate($employeeeasy->date);
-        $this->discount        = $employeeeasy->discount;
-        $this->created         = $employeeeasy->created_at->format('d/m/Y H:i:s');
-    }
-
-    /**
-     * erase()
-     *  exclude()
-     */
-    public function erase(int $employeeeasy_id)
-    {
-        // Férias.
-        $employeeeasy = Employeeeasy::find($employeeeasy_id);
-
-        // Inicializa propriedades dinâmicas.
-        $this->employeeeasy_id = $employeeeasy->id;
-        $this->employee_id     = $employeeeasy->employee_id;
-        $this->employee_name   = $employeeeasy->employee_name;
-        $this->date            = General::decodedate($employeeeasy->date);
-        $this->date_encode     = $employeeeasy->date;
-        $this->discount        = $employeeeasy->discount;
-        $this->created         = $employeeeasy->created_at->format('d/m/Y H:i:s');
-
-        $this->journey         = $employeeeasy->employee->journey;
-    }
-        public function exclude()
-        {
-            // Define $validatedData.
-            $validatedData['employeeeasy_id'] = $this->employeeeasy_id;
-            $validatedData['employee_id']     = $this->employee_id;
-            $validatedData['employee_name']   = $this->employee_name;
-            $validatedData['date']            = $this->date;
-            $validatedData['date_encode']     = $this->date_encode;
-            $validatedData['discount']        = $this->discount;
-            $validatedData['journey']         = $this->journey;
-
-            // Define $data.
-            $data['config']        = $this->config;
-            $data['validatedData'] = $validatedData;
-
-            // Valida exclusão.
-            $valid = Employeeeasy::validateErase($data);
-
-            // Executa dependências.
-            if ($valid) Employeeeasy::dependencyErase($data);
-
-            // Exclui.
-            if ($valid) Employeeeasy::erase($data);
-
-            // Fecha modal.
-            $this->closeModal();
-            $this->dispatchBrowserEvent('close-modal');
-        }
-
-    /**
-     * generate()
-     *  sire()
-     */
-    public function generate()
-    {
-        //...
-    }
-        public function sire()
-        {
-            // Define $data.
-            $data['config'] = $this->config;
-            $data['filter'] = $this->filter;
-            $data['search'] = $this->search;
-
-            // Valida geração de relatório.
-            $valid = Employeeeasy::validateGenerate($data);
-
-            // Gera relatório.
-            if ($valid) Employeeeasy::generate($data);
-
-            // Executa dependências.
-            if ($valid) Employeeeasy::dependencyGenerate($data);
-
-            // Fecha modal.
-            $this->closeModal();
-            $this->dispatchBrowserEvent('close-modal');
-        }
-
-    /**
-     * mail()
-     *  send()
-     */
-    public function mail()
-    {
-        //...
-    }
-        public function send()
-        {
-            // Valida campos.
-            $validatedData = $this->validate([
-                'report_id' => ['required'],
-                'mail'      => ['required', 'email', 'between:2,255'],
-                'comment'   => ['nullable', 'between:2,255'],
-            ]);
-
-            // Define $data
-            $data['config']        = $this->config;
-            $data['validatedData'] = $validatedData;
-
-            // Valida envio do e-mail.
-            $valid = Employeeeasy::validateMail($data);
-
-            // Envia e-mail.
-            if ($valid) Employeeeasy::mail($data);
-
-            // Executa dependências.
-            if ($valid) Employeeeasy::dependencyMail($data);
+            if ($valid) Employeeevent::dependencyAdd($data);
 
             // Fecha modal.
             $this->closeModal();
