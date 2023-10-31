d8944fefa4139019699ca46980109e6a7b512ef1
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2023-10-31 16:16:25 -0300

    sis

--- app/Models/Point.php
+++ app/Models/Point.php
@@ -4,7 +4,72 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Support\Str;
+
 class Point extends Model
 {
+    /**
+     * Nome da tabela.
+     */
+    protected $table = 'point';
+
+    /**
+     * Campos manipuláveis.
+     */
+    protected $fillable = [
+        'company_id',
+        'company_name',
+
+        'start',
+        'end',
+
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * Relaciona Models.
+     */
+    public function company(){return $this->belongsTo(Company::class);}
+
     
+    /**
+     * Valida cadastro TXT.
+     * @var array $data
+     * 
+     * @return <array, bool>
+     */
+    public static function validateAddTxt(array $data){
+        $message = null;
+
+        // ...
+
+        return $txtArray = [];
+    }
+
+    
+    /**
+     * Cadastra TXT.
+     * @var array $data
+     * 
+     * @return bool true
+     */
+    public static function addTxt(array $data) : bool {
+        // ...
+
+        return true;
+    }
+
+    /**
+     * Executa dependências de cadastro TXT.
+     * @var array $data
+     * 
+     * @return bool true
+     */
+    public static function dependencyAddTxt(array $data) : bool {
+        // ...
+
+        return true;
+    }
+
 }
