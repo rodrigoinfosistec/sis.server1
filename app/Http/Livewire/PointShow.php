dcbbe014028edb308f260e369bfbb0813e7f91fe
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2023-10-31 13:59:19 -0300

    sis

---
+++ app/Http/Livewire/PointShow.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class PointShow extends Component
+{
+    public function render()
+    {
+        return view('livewire.point-show');
+    }
+}
