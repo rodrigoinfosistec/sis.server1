6f1a2f9a3a064be494bb4a66a2c0309a28b9d14c
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2023-10-31 16:07:49 -0300

    sis

--- resources/views/livewire/point-show.blade.php
+++ resources/views/livewire/point-show.blade.php
@@ -53,7 +53,6 @@
                 <x-layout.card.card-header-button-more>
 
 {{-- botão add --}}
-<x-layout.card.card-header-button-more-plus/>
 <x-layout.card.card-header-button-more-plus-txt/>
 {{-- botão add --}}
 
@@ -127,131 +126,11 @@
 </x-layout.card.card-body-content-table-body-line-cell>
 
 <x-layout.card.card-body-content-table-body-line-cell-action width="120">
-    @if(App\Models\Clockfunded::where('clock_id', $item->id)->doesntExist())
-        <x-layout.card.card-body-content-table-body-line-cell-action-add-employee :id="$item->id"/>
-
-        <x-layout.card.card-body-content-table-body-line-cell-action-add-holiday :id="$item->id"/>
-
-        <x-layout.card.card-body-content-table-body-line-cell-action-erase-fill :id="$item->id"/>
-
-        @if(App\Models\Clockday::where(['clock_id' => $item->id, 'authorized' => false])->doesntExist())
-            <x-layout.card.card-body-content-table-body-line-cell-action-add-funded :id="$item->id"/>
-        @else
-            <x-layout.card.card-body-content-table-body-line-cell-action-add-funded-banned :id="$item->id"/>
-        @endif
-
-        <x-layout.card.card-body-content-table-body-line-cell-action-mail-funded-muted :id="$item->id"/>
-
-        <x-layout.card.card-body-content-table-body-line-cell-action-print-funded-muted :id="$item"/>
-    @else
-        <x-layout.card.card-body-content-table-body-line-cell-action-add-employee-muted :id="$item->id"/>
-
-        <x-layout.card.card-body-content-table-body-line-cell-action-add-holiday-muted :id="$item->id"/>
-
-        <x-layout.card.card-body-content-table-body-line-cell-action-erase-fill-muted :id="$item->id"/>
-
-        <x-layout.card.card-body-content-table-body-line-cell-action-add-funded-muted :id="$item->id"/>
-
-        <x-layout.card.card-body-content-table-body-line-cell-action-mail-funded :id="$item->id"/>
-
-        <x-layout.card.card-body-content-table-body-line-cell-action-print-funded :id="$item->id"/>
-    @endif
+    
 </x-layout.card.card-body-content-table-body-line-cell-action>
 {{-- conteúdo --}} 
 
                                 </x-layout.card.card-body-content-table-body-line>
-
-                                {{-- nova entidade --}}
-                                <x-layout.card.card-body-content-table>
-                                    <x-layout.card.card-body-content-table-body>
-                                        @php
-                                            $clockemployees = App\Models\Clockemployee::where('clock_id', $item->id)->orderBy('employee_name', 'ASC')->get();
-                                        @endphp
-                                        @if($clockemployees->count() > 0)
-                                            @foreach($clockemployees as $key => $clockemployee)
-                                            <x-layout.card.card-body-content-table-body-line>
-
-{{-- funcionários --}}
-<x-layout.card.card-body-content-table-body-line-cell width="">
-    <x-layout.card.card-body-content-table-body-line-cell-id>
-        <x-layout.card.card-body-content-table-body-line-cell-id-badge>
-            {{ str_pad($loop->iteration, Str::length($clockemployees->count()), '0', STR_PAD_LEFT); }}
-        </x-layout.card.card-body-content-table-body-line-cell-id-badge>
-
-        <x-layout.card.card-body-content-table-body-line-cell-id-start>
-            <span class="text-muted">{{ $clockemployee->employee->pis }}</span>
-        </x-layout.card.card-body-content-table-body-line-cell-id-start>
-
-        <x-layout.card.card-body-content-table-body-line-cell-id-end>
-            {{--  --}}
-        </x-layout.card.card-body-content-table-body-line-cell-id-end>
-    </x-layout.card.card-body-content-table-body-line-cell-id>
-
-    <x-layout.card.card-body-content-table-body-line-cell-content>
-        {{ $clockemployee->employee_name }}
-    </x-layout.card.card-body-content-table-body-line-cell-content>
-</x-layout.card.card-body-content-table-body-line-cell>
-
-<x-layout.card.card-body-content-table-body-line-cell-action width="160">
-    @if(App\Models\Clockemployeefunded::where(['clock_id' => $clockemployee->clock_id, 'employee_id' => $clockemployee->employee_id])->doesntExist())
-        @if(App\Models\Clockday::where(['clock_id' => $clockemployee->clock_id, 'employee_id' => $clockemployee->employee_id, 'authorized' => false])->exists())
-            <x-layout.card.card-body-content-table-body-line-cell-action-edit-clock-employee :id="$clockemployee->id"/>
-        @else
-            <x-layout.card.card-body-content-table-body-line-cell-action-edit-clock-employee-check :id="$clockemployee->id"/>
-        @endif
-        <x-layout.card.card-body-content-table-body-line-cell-action-add-attest-employee :id="$clockemployee->id"/>
-        <x-layout.card.card-body-content-table-body-line-cell-action-add-absence-employee :id="$clockemployee->id"/>
-        <x-layout.card.card-body-content-table-body-line-cell-action-add-vacation-employee :id="$clockemployee->id"/>
-
-        @if(App\Models\Report::where(['folder' => 'clockemployee', 'reference_1' => $clockemployee->clock_id, 'reference_2' => $clockemployee->employee_id])->exists())
-            <x-layout.card.card-body-content-table-body-line-cell-action-mail-employee :id="$clockemployee->id"/>
-        @else
-            <x-layout.card.card-body-content-table-body-line-cell-action-mail-muted-slim/>
-        @endif
-
-        <x-layout.card.card-body-content-table-body-line-cell-action-edit-note-employee :id="$clockemployee->id"/>
-        <x-layout.card.card-body-content-table-body-line-cell-action-add-allowance-employee :id="$clockemployee->id"/>
-        <x-layout.card.card-body-content-table-body-line-cell-action-add-easy-employee :id="$clockemployee->id"/>
-        <x-layout.card.card-body-content-table-body-line-cell-action-erase-employee :id="$clockemployee->id"/>
-        @if(App\Models\Report::where(['folder' => 'clockemployee', 'reference_1' => $clockemployee->clock_id, 'reference_2' => $clockemployee->employee_id])->exists())
-            <x-layout.card.card-body-content-table-body-line-cell-action-print-employee :id="$clockemployee" :reference="$clockemployee->clock_id" :referenca="$clockemployee->employee_id"/>
-        @else
-            <x-layout.card.card-body-content-table-body-line-cell-action-print-muted-slim/>
-        @endif
-    @else
-        <x-layout.card.card-body-content-table-body-line-cell-action-edit-clock-employee-check-muted :id="$clockemployee->id"/>
-        <x-layout.card.card-body-content-table-body-line-cell-action-add-attest-employee-muted :id="$clockemployee->id"/>
-        <x-layout.card.card-body-content-table-body-line-cell-action-add-absence-employee-muted :id="$clockemployee->id"/>
-        <x-layout.card.card-body-content-table-body-line-cell-action-add-vacation-employee-muted :id="$clockemployee->id"/>
-
-        @if(App\Models\Report::where(['folder' => 'clockemployee', 'reference_1' => $clockemployee->clock_id, 'reference_2' => $clockemployee->employee_id])->exists())
-            <x-layout.card.card-body-content-table-body-line-cell-action-mail-employee :id="$clockemployee->id"/>
-        @else
-            <x-layout.card.card-body-content-table-body-line-cell-action-mail-muted-slim/>
-        @endif
-
-        <x-layout.card.card-body-content-table-body-line-cell-action-edit-note-employee-muted :id="$clockemployee->id"/>
-        <x-layout.card.card-body-content-table-body-line-cell-action-add-allowance-employee-muted :id="$clockemployee->id"/>
-        <x-layout.card.card-body-content-table-body-line-cell-action-add-easy-employee-muted :id="$clockemployee->id"/>
-        <x-layout.card.card-body-content-table-body-line-cell-action-erase-employee-muted :id="$clockemployee->id"/>
-
-        @if(App\Models\Report::where(['folder' => 'clockemployee', 'reference_1' => $clockemployee->clock_id, 'reference_2' => $clockemployee->employee_id])->exists())
-            <x-layout.card.card-body-content-table-body-line-cell-action-print-employee :id="$clockemployee" :reference="$clockemployee->clock_id" :referenca="$clockemployee->employee_id"/>
-        @else
-            <x-layout.card.card-body-content-table-body-line-cell-action-print-muted-slim/>
-        @endif
-    @endif
-</x-layout.card.card-body-content-table-body-line-cell-action>
-{{-- funcionários --}}
-
-                                                </x-layout.card.card-body-content-table-body-line>
-                                            @endforeach
-                                        @else
-
-                                        @endif
-                                    </x-layout.card.card-body-content-table-body>
-                                </x-layout.card.card-body-content-table>
-                                    {{-- nova entidade --}}
                             @endforeach
                         @else
                             <x-layout.card.card-body-content-table-body-item-none/>
