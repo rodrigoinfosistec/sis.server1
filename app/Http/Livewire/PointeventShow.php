33a854154000e0620a36539bdf3278735790cecd
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2023-11-01 12:19:31 -0300

    sis

---
+++ app/Http/Livewire/PointeventShow.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class PointeventShow extends Component
+{
+    public function render()
+    {
+        return view('livewire.pointevent-show');
+    }
+}
