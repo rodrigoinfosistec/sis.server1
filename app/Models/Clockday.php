4b99bdc4e896850333f72d9248273a5e3afb4903
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2023-11-08 13:13:49 -0300

    sis

--- app/Models/Clockday.php
+++ app/Models/Clockday.php
@@ -126,54 +126,87 @@ class Clockday extends Model
             if($data['input'] && $data['output']):
                 // Evita saída menor que entrada.
                 if($data['output'] >= $data['input']):
-                    // Define Jornada.
-                    $time_journey   = Clock::intervalMinuts($data['journey_start'], $data['journey_end']);
-                    $j = explode(':', $time_journey);
-                    $minuts_journey = (($j[0] * 60) + $j[1]);
-
-                    // Minutos trabalhados.
-                    $time_day = Clock::intervalMinuts($data['input'], $data['output']);
-                    $t = explode(':', $time_day);
-                    $minuts_work = (($t[0] * 60) + $t[1]);
-
-                    // Tempo trabalhado.
-                    $hour  = $minuts_work / 60;
-                    $hour  = (int)$hour;
-                    $minut = $minuts_work % 60;
-                    $time_work = str_pad($hour, 2 ,'0' , STR_PAD_LEFT) . ':' . str_pad($minut, 2 ,'0' , STR_PAD_LEFT);;
-
-                    // Define os Horários.
+                    // Inicializa variáveis.
+                    $allowance_real = 0;
                     $minuts_delay   = 0;
                     $minuts_extra   = 0;
-                    $minuts_balance = 0;
-                    $time_delay     = '00:00';
-                    $time_extra     = '00:00';
-                    $time_balance   = '00:00';
-                    if($minuts_journey > ($minuts_work + 10)):
-                        $minuts_work += 10;
 
-                        // Atraso.
-                        $minuts_delay = $minuts_journey - $minuts_work;
-                        if($minuts_allowance >= $minuts_delay):
-                            $minuts_delay = 0;
+                    // Converte jornada para minutos.
+                    $minuts_js = General::timeToMinuts($data['journey_start']);
+                    $minuts_je = General::timeToMinuts($data['journey_end']);
+
+                    // Converte Registros em minutos.
+                    $minuts_r_in = General::timeToMinuts($data['input']);
+                    $minuts_r_ou = General::timeToMinuts($data['output']);
+
+                    // verifica se existe abono.
+                    if($minuts_al > 0):
+                        // Verifica se período do abono está fora do expediente.
+                        if(($minuts_as < $minuts_js) && ($minuts_ae >= $minuts_js)):
+                            $minuts_as = $minuts_js;
+                        elseif(($minuts_ae > $minuts_je) && ($minuts_as <= $minuts_je)):
+                            $minuts_ae = $minuts_je;
                         else:
-                            $minuts_delay = $minuts_delay - $minuts_allowance;
+                            $minuts_as = 0;
+                            $minuts_ae = 0;
                         endif;
 
-                        $d_hour  = $minuts_delay / 60;
-                        $d_hour  = (int)$d_hour;
-                        $d_minut = $minuts_delay % 60;
-                        $time_delay = str_pad($d_hour, 2 ,'0' , STR_PAD_LEFT) . ':' . str_pad($d_minut, 2 ,'0' , STR_PAD_LEFT);
-                    elseif($minuts_work > ($minuts_journey + 10)):
-                        $minuts_journey += 10;
-
-                        // Extras.
-                        $minuts_extra = $minuts_work - $minuts_journey;
-                        $e_hour  = $minuts_extra / 60;
-                        $e_hour  = (int)$e_hour;
-                        $e_minut = $minuts_extra % 60;
-                        $time_extra = str_pad($e_hour, 2 ,'0' , STR_PAD_LEFT) . ':' . str_pad($e_minut, 2 ,'0' , STR_PAD_LEFT);
+                        // Abono válido para fins de cálculo.
+                        $minuts_al      = $minuts_ae - $minuts_as;
+                        $allowance_real = $minuts_al;
                     endif;
+
+                    // Analisa Entrada.
+                    if(($minuts_r_in - 5) > $minuts_js):
+                        // Incrementa atraso, caso exista.
+                        $minuts_delay += (($minuts_r_in - 5) - $minuts_js);
+
+                        // Decrementa atraso, no caso abono.
+                        if($minuts_al > 0):
+                            if($minuts_al >= $minuts_delay):
+                                $minuts_al -= $minuts_delay;
+                                $minuts_delay = 0;
+                            else:
+                                $minuts_delay -= $minuts_al;
+                                $minuts_al = 0;
+                            endif;
+                        endif;
+                    elseif(($minuts_r_in + 5) < $minuts_js):
+                        // Incrementa extra.
+                        $minuts_extra += ($minuts_js - ($minuts_r_in + 5));
+                    endif;
+
+                    // Analisa Saída.
+                    if($minuts_je > ($minuts_r_ou + 5)):
+                        // Incrementa atraso, caso exista.
+                        $minuts_delay += ($minuts_je - ($minuts_r_ou + 5));
+
+                        // Decrementa atraso, no caso abono.
+                        if($minuts_al > 0):
+                            if($minuts_al >= $minuts_delay):
+                                $minuts_al -= $minuts_delay;
+                                $minuts_delay = 0;
+                            else:
+                                $minuts_delay -= $minuts_al;
+                                $minuts_allowance = 0;
+                            endif;
+                        endif;
+                    elseif($minuts_je < ($minuts_r_ou - 5)):
+                        // Incrementa extra.
+                        $minuts_extra += (($minuts_r_ou - 5) - $minuts_je);
+                    endif;
+
+                    // Saldo.
+                    $minuts_ba = $minuts_extra - $minuts_delay;
+
+                    // Sinal.
+                    if($minuts_ba > 0):     
+                        $signal = '+';
+                    elseif($minuts_ba < 0): 
+                        $signal = '-';
+                    else:                  
+                         $signal = ''; 
+                    endif;  
                 else:
                     $authorized = false;
                 endif;
@@ -187,7 +220,6 @@ class Clockday extends Model
             if($data['input'] && $data['break_start'] && $data['break_end'] && $data['output']):
                 // Evita pausa inicial menor que entrada.
                 if($data['break_start'] >= $data['input'] && $data['break_end'] >= $data['break_start'] && $data['output'] >= $data['break_end']):
-// --------------------------------------------------------------------------------
                     // Inicializa variáveis.
                     $allowance_real = 0;
                     $minuts_delay   = 0;
