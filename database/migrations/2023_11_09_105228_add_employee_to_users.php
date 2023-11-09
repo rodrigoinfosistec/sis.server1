1e1d4cf1c6a2bb34f9256b37c0d8ae5bb746d338
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2023-11-09 10:58:18 -0300

    sis

---
+++ database/migrations/2023_11_09_105228_add_employee_to_users.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('employee_id')
+                ->after('usergroup_name')
+                ->nullable()->default(null);
+
+            $table->foreign('employee_id')->references('id')->on('employees');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
