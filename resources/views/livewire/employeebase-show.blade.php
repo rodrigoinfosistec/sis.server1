665fbdd47e0d676596e51409337ca017982c7333
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2023-11-09 13:46:22 -0300

    sis

--- resources/views/livewire/employeebase-show.blade.php
+++ resources/views/livewire/employeebase-show.blade.php
@@ -1,112 +1,31 @@
-<x-layout.container>
+<div class="container">
+	<x-layout.alert/>
 
-{{-- modal --}}
-{{-- botões --}}
+	<div id="carouselHome" class="carousel carousel-dark slide carousel-fade" data-bs-ride="carousel">
+		<div class="carousel-indicators" style="margin-bottom: -5px;">
+			@for($i = 0 ; $i < App\Models\Config::getQtdCarousel() ; $i++)
+				<button type="button" data-bs-target="#carouselHome" data-bs-slide-to="{{ $i }}" @if($i == 0) class="active" aria-current="true" @endif aria-label="Slide {{ $i }}"></button>
+			@endfor
+		</div>
 
-{{-- plus --}}
+		<div class="carousel-inner">
+			@for($i = 1 ; $i <= App\Models\Config::getQtdCarousel() ; $i++)
+				<div class="carousel-item @if($i == 1) active @endif">
+					<img src="{{ asset('img/home/carousel/1600/' . $i . '.png?' . Illuminate\Support\Str::random(10)) }}" class="d-block rounded" style="width: 100%; min-height: 180px;"  alt="">
+				</div>
+			@endfor
+		</div>
 
-{{-- info --}}
+		<button class="carousel-control-prev" type="button" data-bs-target="#carouselHome" data-bs-slide="prev">
+			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
 
+			<span class="visually-hidden">Previous</span>
+		</button>
 
-{{-- ações --}}
+		<button class="carousel-control-next" type="button" data-bs-target="#carouselHome" data-bs-slide="next">
+			<span class="carousel-control-next-icon" aria-hidden="true"></span>
 
-{{-- modal --}}
-
-    <x-layout.alert/>
-
-    <x-layout.card.card>
-        <x-layout.card.card-header>
-            <x-layout.card.card-header-button>
-                <x-layout.card.card-header-button-action>
-                    <x-layout.card.card-header-button-action-refresh href="{{ $config['name'] }}"/>
-
-{{-- botão relatório --}}
-@if($existsItem)
-    <x-layout.card.card-header-button-action-generate/>
-@else
-    <x-layout.card.card-header-button-action-generate-muted/>
-@endif
-
-@if($existsReport)
-    <x-layout.card.card-header-button-action-mail/>
-@else
-    <x-layout.card.card-header-button-action-mail-muted/>
-@endif
-{{-- botão relatório --}}
-
-                    @if($existsReport)
-                        <x-layout.card.card-header-button-action-print>
-                            <x-layout.card.card-header-button-action-print-for :config="$config" :reports="$reports"/>
-                        </x-layout.card.card-header-button-print>
-                    @else
-                        <x-layout.card.card-header-button-action-print-muted/>
-                    @endif
-                </x-layout.card.card-header-button-action>
-
-                <x-layout.card.card-header-button-more>
-
-{{-- botão add --}}
-
-{{-- botão add --}}
-
-                </x-layout.card.card-header-button-more>
-            </x-layout.card.card-header-button>
-        </x-layout.card.card-header>
-
-        <x-layout.card.card-body>
-            <x-layout.card.card-body-navigation>
-                <x-layout.card.card-body-navigation-search :filter="$filter">
-                    <x-layout.card.card-body-navigation-search-filter>
-
-{{-- filtro nome --}}
-<option value="pis">PIS</option>
-<option value="name">NOME</option>
-<option value="company_name">EMPRESA</option>
-<option value="created_at">DATA CADASTRO</option>
-{{-- filtro nome --}}
-
-                        </x-layout.card.card-body-navigation-search-filter>
-
-{{-- filtro tipo--}}
-@if($filter == 'created_at')
-    <x-layout.card.card-body-navigation-search-type-date/>
-@else
-    <x-layout.card.card-body-navigation-search-type-search/>
-@endif
-{{-- filtro tipo--}}
-
-                </x-layout.card.card-body-navigation-search>
-
-                <x-layout.card.card-body-navigation-info>
-                    <x-layout.card.card-body-navigation-info-action>
-
-{{-- info action --}}
-
-{{-- info action --}}
-
-                    </x-layout.card.card-body-navigation-info-action>
-
-                    <x-layout.card.card-body-navigation-info-count :count="$list->total()"/>
-                </x-layout.card.card-body-navigation-info>
-            </x-layout.card.card-body-navigation>
-
-            <x-layout.card.card-body-content>
-                <x-layout.card.card-body-content-table>
-                    <x-layout.card.card-body-content-table-body>
-                        @if($list->count() > 0)
-                            @foreach($list as $item)
-                                <x-layout.card.card-body-content-table-body-line>
-{{-- conteúdo --}}  
-
-{{-- conteúdo --}} 
-                                </x-layout.card.card-body-content-table-body-line>
-                            @endforeach
-                        @else
-                            <x-layout.card.card-body-content-table-body-item-none/>
-                        @endif
-                    </x-layout.card.card-body-content-table-body>
-                </x-layout.card.card-body-content-table>
-            </x-layout.card.card-body-content>
-        </x-layout.card.card-body>
-    </x-layout.card.card>
-</x-layout.container>
+			<span class="visually-hidden">Next</span>
+		</button>
+	</div>
+</div>
