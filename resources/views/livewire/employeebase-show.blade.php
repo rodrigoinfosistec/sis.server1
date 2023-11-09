77080e65df84048bcefd72fd76e65f4ed2503655
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2023-11-09 10:50:37 -0300

    sis

--- resources/views/livewire/employeebase-show.blade.php
+++ resources/views/livewire/employeebase-show.blade.php
@@ -1,3 +1,112 @@
-<div>
-    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
-</div>
+<x-layout.container>
+
+{{-- modal --}}
+{{-- botões --}}
+
+{{-- plus --}}
+
+{{-- info --}}
+
+
+{{-- ações --}}
+
+{{-- modal --}}
+
+    <x-layout.alert/>
+
+    <x-layout.card.card>
+        <x-layout.card.card-header>
+            <x-layout.card.card-header-button>
+                <x-layout.card.card-header-button-action>
+                    <x-layout.card.card-header-button-action-refresh href="{{ $config['name'] }}"/>
+
+{{-- botão relatório --}}
+@if($existsItem)
+    <x-layout.card.card-header-button-action-generate/>
+@else
+    <x-layout.card.card-header-button-action-generate-muted/>
+@endif
+
+@if($existsReport)
+    <x-layout.card.card-header-button-action-mail/>
+@else
+    <x-layout.card.card-header-button-action-mail-muted/>
+@endif
+{{-- botão relatório --}}
+
+                    @if($existsReport)
+                        <x-layout.card.card-header-button-action-print>
+                            <x-layout.card.card-header-button-action-print-for :config="$config" :reports="$reports"/>
+                        </x-layout.card.card-header-button-print>
+                    @else
+                        <x-layout.card.card-header-button-action-print-muted/>
+                    @endif
+                </x-layout.card.card-header-button-action>
+
+                <x-layout.card.card-header-button-more>
+
+{{-- botão add --}}
+
+{{-- botão add --}}
+
+                </x-layout.card.card-header-button-more>
+            </x-layout.card.card-header-button>
+        </x-layout.card.card-header>
+
+        <x-layout.card.card-body>
+            <x-layout.card.card-body-navigation>
+                <x-layout.card.card-body-navigation-search :filter="$filter">
+                    <x-layout.card.card-body-navigation-search-filter>
+
+{{-- filtro nome --}}
+<option value="pis">PIS</option>
+<option value="name">NOME</option>
+<option value="company_name">EMPRESA</option>
+<option value="created_at">DATA CADASTRO</option>
+{{-- filtro nome --}}
+
+                        </x-layout.card.card-body-navigation-search-filter>
+
+{{-- filtro tipo--}}
+@if($filter == 'created_at')
+    <x-layout.card.card-body-navigation-search-type-date/>
+@else
+    <x-layout.card.card-body-navigation-search-type-search/>
+@endif
+{{-- filtro tipo--}}
+
+                </x-layout.card.card-body-navigation-search>
+
+                <x-layout.card.card-body-navigation-info>
+                    <x-layout.card.card-body-navigation-info-action>
+
+{{-- info action --}}
+
+{{-- info action --}}
+
+                    </x-layout.card.card-body-navigation-info-action>
+
+                    <x-layout.card.card-body-navigation-info-count :count="$list->total()"/>
+                </x-layout.card.card-body-navigation-info>
+            </x-layout.card.card-body-navigation>
+
+            <x-layout.card.card-body-content>
+                <x-layout.card.card-body-content-table>
+                    <x-layout.card.card-body-content-table-body>
+                        @if($list->count() > 0)
+                            @foreach($list as $item)
+                                <x-layout.card.card-body-content-table-body-line>
+{{-- conteúdo --}}  
+
+{{-- conteúdo --}} 
+                                </x-layout.card.card-body-content-table-body-line>
+                            @endforeach
+                        @else
+                            <x-layout.card.card-body-content-table-body-item-none/>
+                        @endif
+                    </x-layout.card.card-body-content-table-body>
+                </x-layout.card.card-body-content-table>
+            </x-layout.card.card-body-content>
+        </x-layout.card.card-body>
+    </x-layout.card.card>
+</x-layout.container>
