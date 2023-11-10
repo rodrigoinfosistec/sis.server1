2b89b2a07a1e08e72beb7f65ac0e81557dc9df63
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2023-11-10 10:42:49 -0300

    sis

--- resources/views/components/employeebase/modals/mail.blade.php
+++ resources/views/components/employeebase/modals/mail.blade.php
@@ -4,10 +4,10 @@
             <form wire:submit.prevent="send" enctype="multipart/form-data">
                 <div class="modal-header">
                     <div class="modal-title" id="mailModalLabel" style="line-height: 1.1; margin-right: 10px;">
-                        <h5>
-                            <i class="bi-envelope-fill text-secondary" style="font-size: 20px;"></i>
-                            E-mail de {{ $config['title'] }}
-                        </h5>
+                        <h6>
+                            <i class="bi-send-fill text-secondary" style="font-size: 20px;"></i>
+                            SUGESTÃO ANÔNIMA
+                        </h6>
 
                         <div class="text-muted" style="font-size: 8pt;">
                             <x-slot:identifier>
@@ -23,22 +23,6 @@
 {{-- conteúdo --}}
 <div class="row g-3" style="margin-bottom: 10px;">
     <div class="col-sm-12">
-        <label for="mail" style="font-size: 9pt; margin-bottom: -10px;">
-            E-MAIL
-        </label>
-
-        <input type="text" wire:model="mail" class="form-control form-control-sm" id="mail">
-
-        <x-layout.modal.modal-mail-body-group-item-error item="mail" message="$message"/>
-    </div>
-</div>
-
-<div class="row g-3" style="margin-bottom: 10px;">
-    <div class="col-sm-12">
-        <label for="comment" style="font-size: 9pt; margin-bottom: -10px;">
-            SUGESTÃO
-        </label>
-
         <textarea wire:model="comment" class="form-control form-control-sm" id="comment" rows="6"></textarea>
 
         <x-layout.modal.modal-mail-body-group-item-count :comment="$comment"/>
