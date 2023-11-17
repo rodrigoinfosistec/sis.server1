56b16d2c724ae17a2c1190238cb1f19436d7e3dc
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2023-11-17 13:53:08 -0300

    sis

--- resources/views/components/employeebase/modals/add-registry.blade.php
+++ resources/views/components/employeebase/modals/add-registry.blade.php
@@ -1,3 +1,23 @@
-<div>
-    <!-- It is not the man who has too little, but the man who craves more, that is poor. - Seneca -->
-</div>
+<x-layout.modal.modal-add modal="addRegistry" method="registerRegistry" size="">
+    <x-layout.modal.modal-add-header icon="bi-clock-history" modal="addRegistry">
+        Registro de Ponto
+
+        <x-slot:identifier>
+            {{-- ... --}}
+        </x-slot>
+    </x-layout.modal.modal-add-header>
+
+    <x-layout.modal.modal-add-body method="registerRegistry">
+
+{{-- conteúdo --}}
+<x-layout.modal.modal-add-body-group>
+    <x-layout.modal.modal-add-body-group-item columms="12">
+        oi
+    </x-layout.modal.modal-add-body-group-item>
+</x-layout.modal.modal-add-body-group>
+{{-- conteúdo --}}
+
+    </x-layout.modal.modal-add-body>
+
+    <x-layout.modal.modal-add-footer/>
+</x-layout.modal.modal-add>
