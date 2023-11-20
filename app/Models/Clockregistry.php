18dbe5609f2e172020c021b9cc99fc69ef185e85
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2023-11-20 16:36:21 -0300

    sis

--- app/Models/Clockregistry.php
+++ app/Models/Clockregistry.php
@@ -40,9 +40,47 @@ class Clockregistry extends Model
     public static function validateAdd(array $data) : bool {
         $message = null;
 
+        // Funcionário.
+        $employee = Employee::find($data['validatedData']['employee_id']);
+
         // Verifica se este Registro já foi efetuado.
         if(Clockregistry::where(['employee_id' => $data['validatedData']['employee_id'], 'date' => $data['validatedData']['date'], 'time' => $data['validatedData']['time']])->exists()):
             $message = 'Registro já efetuado em ' . date_format(date_create($data['validatedData']['date']), 'd/m/Y') . ' ' . $data['validatedData']['time'] . '.';
+        endif;
+
+        // Verifica se o tipo de registro do Funcionário é 'ALTERNATIVO'.
+        if($employee->clock_type == 'REGISTRY'):
+            $message = 'Sem permissão para utilizar esta função.';
+        endif;
+
+        // Verifica se data é domingo.
+        if($employee->clock_type == 'REGISTRY'):
+            $message = 'Domingo: Dia não autorizado para registrar ponto, será reportado à Gerência.';
+        endif;
+
+        // Verifica se data é Feriado.
+        if($employee->clock_type == 'REGISTRY'):
+            $message = 'Feriado: Dia não autorizado para registrar ponto, será reportado à Gerência.';
+        endif;
+
+        // Verifica se Funcionário está de Férias na data.
+        if($employee->clock_type == 'REGISTRY'):
+            $message = 'Feriado: Dia não autorizado para registrar ponto, será reportado à Gerência.';
+        endif;
+
+        // Verifica se Funcionário está de Licença na data.
+        if($employee->clock_type == 'REGISTRY'):
+            $message = 'Feriado: Dia não autorizado para registrar ponto, será reportado à Gerência.';
+        endif;
+
+        // Verifica se Funcionário está de Atestado na data.
+        if($employee->clock_type == 'REGISTRY'):
+            $message = 'Feriado: Dia não autorizado para registrar ponto, será reportado à Gerência.';
+        endif;
+
+        // Verifica se Funcionário Faltou na data.
+        if($employee->clock_type == 'REGISTRY'):
+            $message = 'Feriado: Dia não autorizado para registrar ponto, será reportado à Gerência.';
         endif;
 
         // Desvio.
