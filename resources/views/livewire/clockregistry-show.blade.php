b5a3993a53c6cf9001eb2a5a50cedf9d6b296661
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2023-11-21 12:58:16 -0300

    sis

--- resources/views/livewire/clockregistry-show.blade.php
+++ resources/views/livewire/clockregistry-show.blade.php
@@ -6,7 +6,7 @@
 @include('components.' .  $config['name'] . '.modals.mail')
 
 {{-- plus --}}
-@include('components.' .  $config['name'] . '.modals.add')
+@include('components.' .  $config['name'] . '.modals.add-registry')
 
 {{-- info --}}
 
