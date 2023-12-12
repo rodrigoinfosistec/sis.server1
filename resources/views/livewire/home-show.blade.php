8f92dac0690c334c48ceb01488e2afeafb1b01d8
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2023-12-12 11:46:21 -0300

    sis

--- resources/views/livewire/home-show.blade.php
+++ resources/views/livewire/home-show.blade.php
@@ -8,10 +8,27 @@
 			@endfor
 		</div>
 
+		@php
+			$link[1] = '#';
+			$link[2] = 'https://www.distribuidoramixpe.com.br/index.php?route=account/register&page=register';
+			$link[3] = 'https://api.whatsapp.com/send/?phone=558130972326&text&type=phone_number&app_absent=0';
+			$link[4] = 'https://www.instagram.com/distribuidora.mix/';
+			$link[5] = '#';
+			$link[6] = '#';
+			$link[7] = '#';
+			$link[8] = '#';
+			$link[9] = '#';
+		@endphp
 		<div class="carousel-inner">
 			@for($i = 1 ; $i <= App\Models\Config::getQtdCarousel() ; $i++)
 				<div class="carousel-item @if($i == 1) active @endif">
-					<img src="{{ asset('img/home/carousel/1600/' . $i . '.png?' . Illuminate\Support\Str::random(10)) }}" class="d-block rounded" style="width: 100%; min-height: 180px;"  alt="">
+					@if(!empty($link[$i]))
+						<a href="{{ $link[$i] }}" @if($link[$i] != '#') target="_BLANK" @endif>
+					@else
+						<a href="#">
+					@endif
+						<img src="{{ asset('img/home/carousel/1600/' . $i . '.png?' . Illuminate\Support\Str::random(10)) }}" class="d-block rounded" style="width: 100%; min-height: 180px;"  alt="">
+					</a>
 				</div>
 			@endfor
 		</div>
