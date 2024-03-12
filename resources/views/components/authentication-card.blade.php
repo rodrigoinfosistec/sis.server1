a46d04204d297d17a912f7f353b76ba2a656beac
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2024-03-12 11:10:42 -0300

    sis

--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -1,4 +1,11 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
+<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900"
+style="
+    background-image: url('/img/background/background.jpg');
+    height: 100%;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+">
     <div>
         {{ $logo }}
     </div>
