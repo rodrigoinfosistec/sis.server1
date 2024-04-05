fea59f3ffe7561174487d04c1d7b1b3b5b75e736
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2024-04-05 16:11:02 -0300

    sis

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ use App\Http\Controllers\ClockregistryController;
 use App\Http\Controllers\ClockregistryemployeeController;
 use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\EmployeebaseController;
+use App\Http\Controllers\RhsearchController;
 use App\Http\Controllers\EmployeevacationController;
 use App\Http\Controllers\EmployeeattestController;
 use App\Http\Controllers\EmployeelicenseController;
@@ -67,6 +68,7 @@ Route::middleware([
     Route::get('/clockregistryemployee', [ClockregistryemployeeController::class, 'index'])->name('clockregistryemployee');
     Route::get('/employee', [EmployeeController::class, 'index'])->name('employee');
     Route::get('/employeebase', [EmployeebaseController::class, 'index'])->name('employeebase');
+    Route::get('/rhsearch', [RhsearchController::class, 'index'])->name('rhsearch');
     Route::get('/employeevacation', [EmployeevacationController::class, 'index'])->name('employeevacation');
     Route::get('/employeeattest', [EmployeeattestController::class, 'index'])->name('employeeattest');
     Route::get('/employeelicense', [EmployeelicenseController::class, 'index'])->name('employeelicense');
