7b41be7bd5216b9a2bccf38b2025fc5af3d41b3c
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2024-04-08 10:01:46 -0300

    sis

--- resources/views/components/rhnews/modals/detail.blade.php
+++ resources/views/components/rhnews/modals/detail.blade.php
@@ -1,3 +1,64 @@
-<div>
-    <!-- It is not the man who has too little, but the man who craves more, that is poor. - Seneca -->
-</div>
+<x-layout.modal.modal-detail modal="detail" size="">
+    <x-layout.modal.modal-detail-header icon="bi-eye" modal="detail">
+        {{ $config['title'] }}
+
+        <x-slot:identifier>
+            {{-- ... --}}
+        </x-slot>
+    </x-layout.modal.modal-detail-header>
+
+    <x-layout.modal.modal-detail-body>
+
+{{-- conteúdo --}}
+<x-layout.modal.modal-detail-body-line>
+    <x-layout.modal.modal-detail-body-line-title>
+        ID
+    </x-layout.modal.modal-detail-body-line-title>
+    <x-layout.modal.modal-detail-body-line-content>
+        {{ $rhnews_id }}
+    </x-layout.modal.modal-detail-body-line-content>
+</x-layout.modal.modal-detail-body-line>
+
+<x-layout.modal.modal-detail-body-line>
+    <x-layout.modal.modal-detail-body-line-title>
+        TÍTULO
+    </x-layout.modal.modal-detail-body-line-title>
+    <x-layout.modal.modal-detail-body-line-content>
+        {{ $name }}
+    </x-layout.modal.modal-detail-body-line-content>
+</x-layout.modal.modal-detail-body-line>
+
+<x-layout.modal.modal-detail-body-line>
+    <x-layout.modal.modal-detail-body-line-title>
+        DESCRIÇÃO
+    </x-layout.modal.modal-detail-body-line-title>
+    <x-layout.modal.modal-detail-body-line-content>
+        {{ $description }}
+    </x-layout.modal.modal-detail-body-line-content>
+</x-layout.modal.modal-detail-body-line>
+
+<x-layout.modal.modal-detail-body-line>
+    <x-layout.modal.modal-detail-body-line-title>
+        STATUS
+    </x-layout.modal.modal-detail-body-line-title>
+    <x-layout.modal.modal-detail-body-line-content>
+        @if($status == true)
+            <span class="text-success">Ativo</span>
+        @else
+            <span class="text-danger">Inativo</span>
+        @endif
+    </x-layout.modal.modal-detail-body-line-content>
+</x-layout.modal.modal-detail-body-line>
+
+<x-layout.modal.modal-detail-body-line>
+    <x-layout.modal.modal-detail-body-line-title>
+        CADASTRO
+    </x-layout.modal.modal-detail-body-line-title>
+    <x-layout.modal.modal-detail-body-line-content>
+        {{ $created }}
+    </x-layout.modal.modal-detail-body-line-content>
+</x-layout.modal.modal-detail-body-line>
+{{-- conteúdo --}}
+
+    </x-layout.modal.modal-detail-body>
+</x-layout.modal.modal-detail>
--- resources/views/components/rhnews/modals/edit.blade.php
+++ resources/views/components/rhnews/modals/edit.blade.php
@@ -1,3 +1,57 @@
-<div>
-    <!-- Live as if you were to die tomorrow. Learn as if you were to live forever. - Mahatma Gandhi -->
+<x-layout.modal.modal-edit modal="edit" size="">
+    <x-layout.modal.modal-edit-header icon="bi-pencil-square" modal="edit">
+        {{ $config['title'] }}
+
+        <x-slot:identifier>
+            ID: {{ $rhnews_id }}
+
+            <i class="bi-caret-right-fill text-muted" style="font-size: 8px;"></i>
+
+            {{ $name }}
+        </x-slot>
+    </x-layout.modal.modal-edit-header>
+
+    <x-layout.modal.modal-edit-body method="modernize">
+
+{{-- conteúdo --}}
+<x-layout.modal.modal-edit-body-group>
+    <x-layout.modal.modal-edit-body-group-item columms="10">
+        <x-layout.modal.modal-edit-body-group-item-label item="name" title="NOME" plus="none"/>
+
+        <input type="text" wire:model="name" class="form-control form-control-sm" id="name">
+
+        <x-layout.modal.modal-edit-body-group-item-error item="name" message="$message"/>
+    </x-layout.modal.modal-edit-body-group-item>
+</x-layout.modal.modal-edit-body-group>
+
+<div class="row g-3" style="margin-bottom: 10px;">
+    <div class="col-sm-12">
+        <textarea wire:model="description" class="form-control form-control-sm" id="description" rows="6"></textarea>
+
+        <x-layout.modal.modal-mail-body-group-item-count :comment="$description"/>
+
+        <x-layout.modal.modal-mail-body-group-item-error item="description" message="$message"/>
+    </div>
 </div>
+
+<x-layout.modal.modal-edit-body-group>
+    <x-layout.modal.modal-edit-body-group-item columms="12">
+        <x-layout.modal.modal-edit-body-group-item-status>
+            <input wire:model="status" class="form-check-input" type="checkbox" role="switch" id="status">
+
+            <x-slot:label>
+                @if($status)
+                    <x-layout.modal.modal-edit-body-group-item-status-active/>
+                @else
+                    <x-layout.modal.modal-edit-body-group-item-status-inactive/>
+                @endif
+            </x-slot>
+        </x-layout.modal.modal-edit-body-group-item-status>
+    </x-layout.modal.modal-edit-body-group-item>
+</x-layout.modal.modal-edit-body-group>
+{{-- conteúdo --}}
+
+    </x-layout.modal.modal-edit-body>
+
+    <x-layout.modal.modal-edit-footer/>
+</x-layout.modal.modal-edit>
