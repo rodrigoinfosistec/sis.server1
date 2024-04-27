9db4bc1ef8e3f58d0210e441e6f56cef138c6c7f
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2024-04-27 09:24:16 -0300

    sis

--- resources/views/components/layout/button/whatsapp.blade.php
+++ resources/views/components/layout/button/whatsapp.blade.php
@@ -1,4 +1,4 @@
 <a type="button" @if(App\Models\Support::getWhatsApp() != '#') target="_blank" @endif href="{{ App\Models\Support::getWhatsApp() }}"
  class="btn btn-link position-fixed opacity-75 rounded-circle" style="background-color: #25D366; padding: 0px 2px 0px 2px; right: 20px; bottom: 25px;" title="Suporte">
-    <i class="bi-whatsapp text-white" style="font-size: 20pt; padding: 0 4px 0 4px;"></i>
+    <i class="bi-whatsapp text-white" style="font-size: 22pt; padding: 0 5px 0 5px;"></i>
 </a>
