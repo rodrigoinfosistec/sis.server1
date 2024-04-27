e63e4a31fca47e63cd35389de11672821514f1ac
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2024-04-27 09:05:15 -0300

    sis

--- resources/views/components/layout/button/up.blade.php
+++ resources/views/components/layout/button/up.blade.php
@@ -1,3 +1,3 @@
 <a href="#" class="btn btn-dark text-white rounded position-fixed opacity-75" style="
-    left: 20px; bottom: 10px; padding: 10px 15px 10px 15px; hover:background: #d3d3d3;
+    left: 20px; bottom: 25px; padding: 7px 10px 8px 10px; hover:background: #d3d3d3;
 ">&#9650;</a>
--- resources/views/components/layout/button/whatsapp.blade.php
+++ resources/views/components/layout/button/whatsapp.blade.php
@@ -1,4 +1,4 @@
 <a type="button" @if(App\Models\Support::getWhatsApp() != '#') target="_blank" @endif href="{{ App\Models\Support::getWhatsApp() }}"
- class="btn btn-link position-fixed opacity-75 bg-success rounded-circle" style="padding: 0px 2px 0px 2px; right: 20px; bottom: 10px;" title="Suporte">
-    <i class="bi-whatsapp text-white" style="font-size: 25pt; padding: 0 6px 0 6px;"></i>
+ class="btn btn-link position-fixed opacity-75 rounded-circle" style="background-color: #25D366; padding: 0px 2px 0px 2px; right: 20px; bottom: 25px;" title="Suporte">
+    <i class="bi-whatsapp text-white" style="font-size: 20pt; padding: 0 4px 0 4px;"></i>
 </a>
