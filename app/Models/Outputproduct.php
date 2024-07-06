28b9e2c9cbc4a011d0d8c5f26136a25948a63363
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2024-07-06 10:09:34 -0300

    sis

---
+++ app/Models/Outputproduct.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Outputproduct extends Model
+{
+    use HasFactory;
+}
