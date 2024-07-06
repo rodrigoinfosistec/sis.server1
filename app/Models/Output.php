56a855e044d6416df980d61b8492601e7084a437
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2024-07-06 10:14:53 -0300

    sis

--- app/Models/Output.php
+++ app/Models/Output.php
@@ -5,7 +5,43 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Support\Str;
+
 class Output extends Model
 {
     use HasFactory;
+
+    /**
+     * Nome da tabela.
+     */
+    protected $table = 'outputs';
+
+    /**
+     * Campos manipuláveis.
+     */
+    protected $fillable = [
+        'deposit_id',
+        'deposit_name',
+
+        'company_id',
+
+        'user_id',
+        'user_name',
+
+        'observation',
+
+        'finished',
+
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * Relaciona Models.
+     */
+    public function deposit(){return $this->belongsTo(Deposit::class);}
+    public function company(){return $this->belongsTo(Company::class);}
+    public function user(){return $this->belongsTo(User::class);}
+
+
 }
--- app/Models/Outputproduct.php
+++ app/Models/Outputproduct.php
@@ -8,4 +8,28 @@ use Illuminate\Database\Eloquent\Model;
 class Outputproduct extends Model
 {
     use HasFactory;
+
+    /**
+     * Nome da tabela.
+     */
+    protected $table = 'outputproducts';
+
+    /**
+     * Campos manipuláveis.
+     */
+    protected $fillable = [
+        'output_id',
+        'product_id',
+
+        'quantity',
+
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * Relaciona Models.
+     */
+    public function output(){return $this->belongsTo(Output::class);}
+    public function product(){return $this->belongsTo(Product::class);}
 }
