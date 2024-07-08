8bb3c31b62e0250b834d02df2ca2a8794aece609
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2024-07-08 08:35:38 -0300

    sis

--- app/Models/Outputproduct.php
+++ app/Models/Outputproduct.php
@@ -32,4 +32,68 @@ class Outputproduct extends Model
      */
     public function output(){return $this->belongsTo(Output::class);}
     public function product(){return $this->belongsTo(Product::class);}
+
+    /**
+     * Valida cadastro.
+     * @var array $data
+     * 
+     * @return <object, bool>
+     */
+    public static function validateAdd(array $data){
+        $message = null;
+
+        // Verifica se o Produto não está cadastrado na Saída.
+        if(Outputproduct::where(['output_id' => $data['validatedData']['output_id'], 'product_id' => $data['validatedData']['product_id']])->exists()):
+            $message = 'Produto já cadastrado nesta saída.';
+        endif;
+
+        // Desvio.
+        if(!empty($message)):
+            session()->flash('message', $message );
+            session()->flash('color', 'danger');
+
+            return false;
+        endif;
+
+        return true;
+    }
+
+    /**
+     * Cadastra.
+     * @var array $data
+     * 
+     * @return bool true
+     */
+    public static function add(array $data) {
+        $outputproduct_id = Outputproduct::create([
+            'outout_id' => $data['validatedData']['output_id'],
+            'product_id' => $data['validatedData']['product_id'],
+            'quantity' => $data['validatedData']['quantity'],
+        ])->id;
+
+        // After.
+        $after = Output::find($outputproduct_id);
+
+        // Auditoria.
+        //Audit::outputAdd($data, $after);
+
+        // Mensagem.
+        $message = 'Produto ' . $after->product->name . ' incluído na Saída.';
+        session()->flash('message', $message);
+        session()->flash('color', 'success');
+
+        return $output_id;
+    }
+
+    /**
+     * Executa dependências de cadastro.
+     * @var array $data
+     * 
+     * @return bool true
+     */
+    public static function dependencyAdd(array $data) : bool {
+        // ...
+
+        return true;
+    }
 }
