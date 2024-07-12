ece287a215319ff4389d4f1aa82cee06b4a50e9d
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2024-07-12 10:16:06 -0300

    sis

---
+++ resources/views/components/layout/card/card-body-content-table-body-line-cell-action-erase-depositoutputproduct.blade.php
@@ -0,0 +1,3 @@
+<a type="button" wire:click="eraseProduct({{ $id }})" class="btn btn-link btn-sm" style="padding:0; margin: 0;" data-bs-toggle="modal" data-bs-target="#eraseProductModal" title="Excluir Produto">
+    <i class="bi-trash3 text-danger" style="font-size: 12px;"></i>
+</a>
--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -32,10 +32,17 @@
                         </x-nav-link>
                     </div>
 
-                    {{-- Transferêcnia Depósitos --}}
+                    {{-- Transferêcnia Depósito --}}
                     <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                        <x-nav-link href="{{ route('deposittransfer') }}" :active="request()->routeIs('deposittransfer')" class="text-decoration-none" title="Transferência Depósitos">
+                        <x-nav-link href="{{ route('deposittransfer') }}" :active="request()->routeIs('deposittransfer')" class="text-decoration-none" title="Transferência Depósito">
                             <i class="bi-arrow-left-right" style="font-size: 20pt;"></i>
+                        </x-nav-link>
+                    </div>
+
+                    {{-- Saída Depósito --}}
+                    <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                        <x-nav-link href="{{ route('depositoutput') }}" :active="request()->routeIs('depositoutput')" class="text-decoration-none" title="Saída Depósito">
+                            <i class="bi-arrow-down" style="font-size: 20pt;"></i>
                         </x-nav-link>
                     </div>
                 @endif
@@ -194,6 +201,11 @@
             {{-- Transferência Depósito --}}
             <x-nav-link href="{{ route('deposittransfer') }}" :active="request()->routeIs('deposittransfer')" class="text-decoration-none" style="margin-right: 10px;">
                 <i class="bi-arrow-left-right" style="font-size: 20pt;"></i>
+            </x-nav-link>
+
+            {{-- Saída Depósito --}}
+            <x-nav-link href="{{ route('depositoutput') }}" :active="request()->routeIs('depositoutput')" class="text-decoration-none" style="margin-right: 10px;">
+                <i class="bi-arrow-down" style="font-size: 20pt;"></i>
             </x-nav-link>
         </div>
 
