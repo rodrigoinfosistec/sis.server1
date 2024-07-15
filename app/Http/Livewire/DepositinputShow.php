10507a809a301e95c44e89f2aa23b3ed1cf0b3d5
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2024-07-15 15:16:29 -0300

    sis

---
+++ app/Http/Livewire/DepositinputShow.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class DepositinputShow extends Component
+{
+    public function render()
+    {
+        return view('livewire.depositinput-show');
+    }
+}
