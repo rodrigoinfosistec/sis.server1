219082781fe6e095cb33b8e504c3d28adff7bdb2
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2024-07-15 16:55:17 -0300

    sis

--- app/Models/Depositinput.php
+++ app/Models/Depositinput.php
@@ -5,7 +5,49 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Support\Str;
+
 class Depositinput extends Model
 {
     use HasFactory;
+
+    /**
+     * Nome da tabela.
+     */
+    protected $table = 'depositinputs';
+
+    /**
+     * Campos manipuláveis.
+     */
+    protected $fillable = [
+        'deposit_name',
+        'deposit_id',
+
+        'provider_id',
+        'provider_name',
+
+        'company_id',
+        'company_name',
+
+        'user_id',
+        'user_name',
+
+        'key',
+        'number',
+        'range',
+        'total',
+
+        'issue',
+
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * Relaciona Models.
+     */
+    public function deposit(){return $this->belongsTo(Deposit::class);}
+    public function provider(){return $this->belongsTo(Provider::class);}
+    public function company(){return $this->belongsTo(Company::class);}
+    public function user(){return $this->belongsTo(User::class);}
 }
