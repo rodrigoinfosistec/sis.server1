584b148efce4dfb211b407c7c498394ec74ef014
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2024-07-15 16:27:45 -0300

    sis

--- database/migrations/2024_07_15_144349_create_depositinputs_table.php
+++ database/migrations/2024_07_15_144349_create_depositinputs_table.php
@@ -14,15 +14,21 @@ return new class extends Migration
         Schema::create('depositinputs', function (Blueprint $table) {
             $table->id();
 
-            $table->string('provider_name');
+            $table->string('deposit_name');
+            $table->unsignedBigInteger('deposit_id');
+
             $table->unsignedBigInteger('provider_id');
+            $table->string('provider_name');
 
             $table->unsignedBigInteger('company_id');
             $table->string('company_name');
 
-            $table->string('key')->unique();
+            $table->unsignedBigInteger('user_id');
+            $table->string('user_name');
+
+            $table->string('key')->unique()->nullable()->default(null);
             $table->string('number');
-            $table->string('range');
+            $table->string('range')->default('000');
 
             $table->decimal('total', 12, 3);
 
@@ -30,8 +36,10 @@ return new class extends Migration
 
             $table->timestamps();
 
+            $table->foreign('deposit_id')->references('id')->on('deposits');
             $table->foreign('provider_id')->references('id')->on('providers');
             $table->foreign('company_id')->references('id')->on('companies');
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
