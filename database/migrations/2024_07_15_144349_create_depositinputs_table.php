dc5e7e949d8c6b845588f8cdc274d9e50801da65
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2024-07-15 15:53:41 -0300

    sis

--- database/migrations/2024_07_15_144349_create_depositinputs_table.php
+++ database/migrations/2024_07_15_144349_create_depositinputs_table.php
@@ -13,7 +13,25 @@ return new class extends Migration
     {
         Schema::create('depositinputs', function (Blueprint $table) {
             $table->id();
+
+            $table->string('provider_name');
+            $table->unsignedBigInteger('provider_id');
+
+            $table->unsignedBigInteger('company_id');
+            $table->string('company_name');
+
+            $table->string('key')->unique();
+            $table->string('number');
+            $table->string('range');
+
+            $table->decimal('total', 12, 3);
+
+            $table->timestamp('issue');
+
             $table->timestamps();
+
+            $table->foreign('provider_id')->references('id')->on('providers');
+            $table->foreign('company_id')->references('id')->on('companies');
         });
     }
 
