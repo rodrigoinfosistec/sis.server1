ffc3d3a4cc5b4da9e8a257105afbd54ec7a3c29e
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2024-07-17 11:40:33 -0300

    sis

--- app/Http/Livewire/DepositinputShow.php
+++ app/Http/Livewire/DepositinputShow.php
@@ -56,8 +56,91 @@ class DepositinputShow extends Component
 
     public $xml;
 
-    public function render()
+    /**
+     * Construtor.
+     */
+    public function mount($config){
+        $this->config = $config;
+    }
+
+    /**
+     * Valida campos gerais.
+     */
+    protected function rules()
     {
-        return view('livewire.depositinput-show');
+        return [
+            'report_id' => ['required'],
+            'mail'      => ['required', 'email', 'between:2,255'],
+            'comment'   => ['nullable', 'between:2,255'],
+
+            'xml' => ['file', 'required'],
+        ];
+    }
+
+    /**
+     * Valida atualização.
+     */
+    public function updated($fields){
+        $this->validateOnly($fields);
+    }
+
+    /**
+     * Fecha Modals.
+     */
+    public function closeModal(){
+        $this->resetInput();
+        $this->resetValidation();
+    }
+
+    /**
+     * Reseta atributos.
+     */
+    public function resetInput()
+    {
+        $this->report_id = '';
+        $this->mail = '';
+        $this->comment = '';
+
+        $this->depositinput_id = '';
+        $this->deposit_name = '';
+        $this->deposit_id = '';
+        $this->provider_id = '';
+        $this->provider_name = '';
+        $this->company_id = '';
+        $this->company_name = '';
+        $this->user_id = '';
+        $this->user_name = '';
+        $this->key = '';
+        $this->number = '';
+        $this->range = '';
+        $this->total = '';
+        $this->issue = '';
+        $this->created = '';
+
+        $this->xml = '';
+    }
+
+    /**
+     * Atualiza conteúdo sem atualizar página.
+     */
+    public function refresh()
+    {
+        $this->emit('refreshChildren');
+    }
+
+    /**
+     * Renderiza página.
+     */
+    public function render(){
+        return view('livewire.' . $this->config['name'] . '-show', [
+            'config'       => $this->config,
+            'existsItem'   => Depositinput::exists(),
+            'existsReport' => Report::where(['folder' => $this->config['name'], 'reference_1' => Auth()->user()->company_id])->exists(),
+            'reports'      => Report::where(['folder' => $this->config['name'], 'reference_1' => Auth()->user()->company_id])->orderBy('id', 'DESC')->limit(12)->get(),
+            'list'         => Invoice::where([
+                                [$this->filter, 'like', '%'. $this->search . '%'],
+                                ['company_id', Auth()->user()->company_id],
+                            ])->orderBy('id', 'DESC')->paginate(25),
+        ]);
     }
 }
