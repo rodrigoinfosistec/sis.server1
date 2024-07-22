858e8a9108b919ff1b4b635774e37c4746b6c76b
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2024-07-22 15:30:02 -0300

    sis

--- app/Models/Depositinputproduct.php
+++ app/Models/Depositinputproduct.php
@@ -8,4 +8,30 @@ use Illuminate\Database\Eloquent\Model;
 class Depositinputproduct extends Model
 {
     use HasFactory;
+
+    /**
+     * Nome da tabela.
+     */
+    protected $table = 'depositinputproducts';
+
+    /**
+     * Campos manipuláveis.
+     */
+    protected $fillable = [
+        'depositinput_id',
+
+        'product_id',
+        'product_name',
+
+        'quantity',
+
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * Relaciona Models.
+     */
+    public function depositinput(){return $this->belongsTo(Depositinput::class);}
+    public function product(){return $this->belongsTo(Product::class);}
 }
