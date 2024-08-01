1021ef56237dab86d5f0a78994510a9b90f52286
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2024-08-01 16:53:15 -0300

    sis

--- app/Models/Report.php
+++ app/Models/Report.php
@@ -1845,4 +1845,41 @@ class Report extends Model
 
         return  $xml;
     }
+
+    /**
+     * Depositinput Generate
+     * @var array $data
+     * 
+     * @return bool true
+     */
+    public static function depositinputGenerate(array $data) : bool {
+        // Define as variáveis
+        $path = public_path('/storage/pdf/' . $data['config']['name'] . '/');
+        $file_name = 'depositinput_' . $data['validatedData']['depositinput_id'] . '_' . auth()->user()->id . '_' . Str::random(20) . '.pdf';
+
+        // Gera o arquivo PDF.
+        $pdf = PDF::loadView('components.' . $data['config']['name'] . '.pdf', [
+            'user'  => auth()->user()->name,
+            'title' => 'Entrada',
+            'date'  => date('d/m/Y H:i:s'),
+            'depositinput' => Depositinput::find($data['validatedData']['depositinput_id']),
+            'list'  => $list = Depositinputproduct::where(
+                'depositinput_id', $data['validatedData']['depositinput_id']
+            )->get(), 
+        ])->set_option('isPhpEnabled', true)->setPaper('A4', 'portrait');
+
+        // Salva o arquivo PDF.
+        File::makeDirectory($path, $mode = 0777, true, true);
+        $pdf->save($path . $file_name);
+
+        // Registra os dados do arquivo PDF.
+        Report::create([
+            'user_id' => auth()->user()->id,
+            'folder'  => $data['config']['name'],
+            'file'    => $file_name,
+            'reference_1' => $data['validatedData']['depositinput_id'],
+        ]);
+
+        return true;
+    }
 }
