d273eb206f30814462c893b44b58ec50d2cd5bc7
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2024-09-25 11:05:24 -0300

    sis

---
+++ app/Http/Livewire/Employeepresencein.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Employeepresencein extends Component
+{
+    public function render()
+    {
+        return view('livewire.employeepresencein');
+    }
+}
