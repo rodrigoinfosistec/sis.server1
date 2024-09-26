d61a2b6cd732852df4c421a21eb96ac7d7e4fe8d
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2024-09-26 11:37:50 -0300

    sis

--- resources/views/components/layout/card/card-body-content-table-body-line-cell-action-edit-employee.blade.php
+++ resources/views/components/layout/card/card-body-content-table-body-line-cell-action-edit-employee.blade.php
@@ -1,3 +1,12 @@
-<a type="button" wire:click="editEmployee({{ $id }})" class="btn btn-link btn-sm" style="padding: 0px 5px 0px 5px;" data-bs-toggle="modal" data-bs-target="#editEmployeeModal" title="Editar Presença Entrada">
+<a type="button" wire:click="editEmployee({{ $id }})" class="btn btn-link btn-sm position-relative" style="padding: 0px 5px 0px 5px;" data-bs-toggle="modal" data-bs-target="#editEmployeeModal" title="Editar Presença Entrada">
     <i class="bi-people text-primary" style="font-size: 20px;"></i>
+    <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill
+    @if(App\Models\Presenceinemployee::where(['presencein_id' => $id, 'is_present' => false])->exists())
+        bg-danger
+    @endif
+    ">
+    @if(App\Models\Presenceinemployee::where(['presencein_id' => $id, 'is_present' => false])->exists())
+        {{ App\Models\Presenceinemployee::where(['presencein_id' => $id, 'is_present' => false])->count() }}
+    @endif
+    </span>
 </a>
