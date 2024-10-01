a388e7d002b27ce3860c0a01c20d0cd2026c1923
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2024-10-01 09:49:12 -0300

    sis

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -51,40 +51,40 @@
                                     <span class="inline-flex rounded-md">
                                         <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none focus:bg-gray-50 dark:focus:bg-gray-700 active:bg-gray-50 dark:active:bg-gray-700 transition ease-in-out duration-150">
                                             {{ Auth::user()->currentTeam->name }}
-
+    
                                             <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                                                 <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 15L12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
                                             </svg>
                                         </button>
                                     </span>
                                 </x-slot>
-
+    
                                 <x-slot name="content">
                                     <div class="w-60">
                                         {{-- Team Management --}}
                                         <div class="block px-4 py-2 text-xs text-gray-400">
                                             {{ __('Manage Team') }}
                                         </div>
-
+    
                                         {{-- Team Settings --}}
                                         <x-dropdown-link href="{{ route('teams.show', Auth::user()->currentTeam->id) }}">
                                             {{ __('Team Settings') }}
                                         </x-dropdown-link>
-
+    
                                         @can('create', Laravel\Jetstream\Jetstream::newTeamModel())
                                             <x-dropdown-link href="{{ route('teams.create') }}">
                                                 {{ __('Create New Team') }}
                                             </x-dropdown-link>
                                         @endcan
-
+    
                                         {{-- Team Switcher --}}
                                         @if (Auth::user()->allTeams()->count() > 1)
                                             <div class="border-t border-gray-200 dark:border-gray-600"></div>
-
+    
                                             <div class="block px-4 py-2 text-xs text-gray-400">
                                                 {{ __('Switch Teams') }}
                                             </div>
-
+    
                                             @foreach (Auth::user()->allTeams() as $team)
                                                 <x-switchable-team :team="$team" />
                                             @endforeach
@@ -94,7 +94,7 @@
                             </x-dropdown>
                         </div>
                     @endif
-
+    
                     {{-- Settings Dropdown --}}
                     <div class="ml-3 relative">
                         <x-dropdown align="right" width="48">
@@ -108,7 +108,7 @@
                                     <span class="inline-flex rounded-md">
                                         <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none focus:bg-gray-50 dark:focus:bg-gray-700 active:bg-gray-50 dark:active:bg-gray-700 transition ease-in-out duration-150">
                                             <span class="text-capitalize">{{ Auth::user()->name }}</span>
-
+    
                                             <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                                                 <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
                                             </svg>
@@ -116,42 +116,46 @@
                                     </span>
                                 @endif
                             </x-slot>
-
+    
                             <x-slot name="content">
                                 {{-- Account Management --}}
                                 <div class="block px-4 py-2 text-xs text-gray-400">
                                     {{ __('Manage Account') }}
                                 </div>
-
+    
                                 <x-dropdown-link href="{{ route('profile.show') }}" class="text-decoration-none">
                                     <i class="bi-gear-fill"></i> {{ __('Profile') }}
                                 </x-dropdown-link>
-
+    
+                                <x-dropdown-link href="{{ route('employeebase') }}" class="text-decoration-none">
+                                    <i class="{{ App\Models\Page::getIconByName('employeebase') }}"></i> {{ App\Models\Page::getTitleByName('employeebase') }}
+                                </x-dropdown-link>
+    
                                 <x-dropdown-link href="{{ route('usergroup') }}" class="text-decoration-none">
                                     <i class="{{ App\Models\Page::getIconByName('usergroup') }}"></i> {{ App\Models\Page::getTitleByName('usergroup') }}
                                 </x-dropdown-link>
-
+    
                                 <x-dropdown-link href="{{ route('user') }}" class="text-decoration-none">
                                     <i class="{{ App\Models\Page::getIconByName('user') }}"></i> {{ App\Models\Page::getTitleByName('user') }}
                                 </x-dropdown-link>
-
+    
                                 <x-dropdown-link href="{{ route('audit') }}" class="text-decoration-none">
                                     <i class="{{ App\Models\Page::getIconByName('audit') }}"></i> {{ App\Models\Page::getTitleByName('audit') }}
                                 </x-dropdown-link>
-
+    
                                 @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
                                     <x-dropdown-link href="{{ route('api-tokens.index') }}">
                                         {{ __('API Tokens') }}
                                     </x-dropdown-link>
                                 @endif
-
+    
                                 <div class="border-t border-gray-200 dark:border-gray-600"></div>
-
+    
                                 {{-- Authentication --}}
                                 <form method="POST" action="{{ route('logout') }}" x-data>
                                     @csrf
-
-                                    <x-dropdown-link href="{{ route('logout') }}" @click.prevent="$root.submit();" class="text-danger text-decoration-none">
+    
+                                    <x-dropdown-link href="{{ route('logout') }}" @click.prevent="$root.submit();" class=" text-danger text-decoration-none">
                                         <i class="bi-box-arrow-left"></i> {{ __('Log Out') }}
                                     </x-dropdown-link>
                                 </form>
@@ -159,7 +163,7 @@
                         </x-dropdown>
                     </div>
                 </div>
-
+    
                 {{-- Hamburger --}}
                 <div class="-mr-2 flex items-center sm:hidden">
                     <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out">
@@ -181,13 +185,13 @@
                 <i class="bi-receipt" style="font-size: 20pt;"></i>
             </x-nav-link>
 
-            {{-- Ponto --}}
-            <x-nav-link href="{{ route('clock') }}" :active="request()->routeIs('clock')" class="text-decoration-none" style="margin-right: 10px;">
+             {{-- Ponto --}}
+             <x-nav-link href="{{ route('clock') }}" :active="request()->routeIs('clock')" class="text-decoration-none" style="margin-right: 10px;">
                 <i class="bi-clock" style="font-size: 20pt;"></i>
             </x-nav-link>
 
-			{{-- Entrada Depósito --}}
-            <x-nav-link href="{{ route('depositinput') }}" :active="request()->routeIs('depositinput')" class="text-decoration-none" style="margin-right: 10px;">
+            {{-- Entrada Depósito --}}
+             <x-nav-link href="{{ route('depositinput') }}" :active="request()->routeIs('depositinput')" class="text-decoration-none" style="margin-right: 10px;">
                 <i class="bi-plus-square-dotted" style="font-size: 20pt;"></i>
             </x-nav-link>
 
@@ -217,6 +221,10 @@
                 {{-- Account Management --}}
                 <x-responsive-nav-link href="{{ route('profile.show') }}" :active="request()->routeIs('profile.show')" class="text-decoration-none">
                     <i class="bi-gear-fill"></i> {{ __('Profile') }}
+                </x-responsive-nav-link>
+
+                <x-responsive-nav-link href="{{ route('employeebase') }}" :active="request()->routeIs('employeebase')" class="text-decoration-none">
+                    <i class="{{ App\Models\Page::getIconByName('employeebase') }}"></i> {{ App\Models\Page::getTitleByName('employeebase') }}
                 </x-responsive-nav-link>
 
                 <x-responsive-nav-link href="{{ route('usergroup') }}" :active="request()->routeIs('usergroup')" class="text-decoration-none">
