0b7769fa49df46265772b5066cc7e815500224f3
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2024-10-04 08:38:01 -0300

    sis

--- resources/views/components/employeepoint/modals/edit-employeegroup.blade.php
+++ resources/views/components/employeepoint/modals/edit-employeegroup.blade.php
@@ -10,48 +10,7 @@
     <x-layout.modal.modal-edit-body method="modernizeEmployeegroup">
 
 {{-- conteúdo --}}
-<x-layout.modal.modal-edit-body-group>
-    <x-layout.modal.modal-edit-body-group-item columms="6">
-        <x-layout.modal.modal-edit-body-group-item-label item="cnpj" title="CNPJ" plus="none"/>
-
-        <input type="text" wire:model="cnpj" class="form-control form-control-sm" id="cnpj" onKeyUp="maskCnpj(this, event)" disabled>
-
-        <x-layout.modal.modal-edit-body-group-item-error item="cnpj" message="$message"/>
-    </x-layout.modal.modal-edit-body-group-item>
-
-    <x-layout.modal.modal-edit-body-group-item columms="6">
-        <x-layout.modal.modal-edit-body-group-item-label item="price" title="PREÇO TIPO" plus="none"/>
-
-        <select wire:model="price" class="form-select form-select-sm text-uppercase" id="price">
-            <x-layout.modal.modal-edit-body-group-item-option-muted/>
-
-            <option value="1">1</option>
-            <option value="2">2</option>
-        </select>
-
-        <x-layout.modal.modal-edit-body-group-item-error item="price" message="$message"/>
-    </x-layout.modal.modal-edit-body-group-item>
-</x-layout.modal.modal-edit-body-group>
-
-<x-layout.modal.modal-edit-body-group>
-    <x-layout.modal.modal-edit-body-group-item columms="12">
-        <x-layout.modal.modal-edit-body-group-item-label item="name" title="RAZÃO SOCIAL" plus="none"/>
-
-        <input type="text" wire:model="name" class="form-control form-control-sm" id="name">
-
-        <x-layout.modal.modal-edit-body-group-item-error item="name" message="$message"/>
-    </x-layout.modal.modal-edit-body-group-item>
-</x-layout.modal.modal-edit-body-group>
-
-<x-layout.modal.modal-edit-body-group>
-    <x-layout.modal.modal-edit-body-group-item columms="12">
-        <x-layout.modal.modal-edit-body-group-item-label item="nickname" title="NOME FANTASIA" plus="none"/>
-
-        <input type="text" wire:model="nickname" class="form-control form-control-sm" id="nickname">
-
-        <x-layout.modal.modal-edit-body-group-item-error item="nickname" message="$message"/>
-    </x-layout.modal.modal-edit-body-group-item>
-</x-layout.modal.modal-edit-body-group>
+    
 {{-- conteúdo --}}
 
     </x-layout.modal.modal-edit-body>
