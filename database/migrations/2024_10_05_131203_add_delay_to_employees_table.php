1111b9d341dbefa6a0cb9e4640d29d2d2eb92ac3
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2024-10-05 13:14:31 -0300

    sis

---
+++ database/migrations/2024_10_05_131203_add_delay_to_employees_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('employees', function (Blueprint $table) {
+            $table->integer('limit_delay')
+                ->after('limit_end_saturday')
+                ->default(60);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('employees', function (Blueprint $table) {
+            //
+        });
+    }
+};
