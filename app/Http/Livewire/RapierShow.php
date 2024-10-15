641c7a9387f10a168877cfa863435892f6464255
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2024-10-15 15:27:19 -0300

    sis

---
+++ app/Http/Livewire/RapierShow.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class RapierShow extends Component
+{
+    public function render()
+    {
+        return view('livewire.rapier-show');
+    }
+}
