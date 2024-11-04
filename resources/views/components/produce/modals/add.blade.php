a44e6b144dc51c6c2ed642d359c42b2013641a6d
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2024-11-04 14:46:07 -0300

    sis

--- resources/views/components/produce/modals/add.blade.php
+++ resources/views/components/produce/modals/add.blade.php
@@ -11,7 +11,21 @@
 
 {{-- conteúdo --}}
 <x-layout.modal.modal-add-body-group>
-    <x-layout.modal.modal-add-body-group-item columms="6">
+    <x-layout.modal.modal-add-body-group-item columms="4">
+        <x-layout.modal.modal-add-body-group-item-label item="deposit_id" title="DEPÓSITO" plus="deposit"/>
+
+        <select wire:model="deposit_id" class="form-select form-select-sm text-uppercase" id="deposit_id">
+            <x-layout.modal.modal-add-body-group-item-option-muted/>
+
+            @foreach(App\Models\Deposit::where(['company_id'=>auth()->user()->company_id, 'status'=>true])->orderBy('name', 'ASC')->get() as $key => $deposit)
+                <option value="{{ $deposit->id }}">{{ $deposit->name }}</option>
+            @endforeach
+        </select>
+
+        <x-layout.modal.modal-add-body-group-item-error item="deposit_id" message="$message"/>
+    </x-layout.modal.modal-add-body-group-item>
+
+    <x-layout.modal.modal-add-body-group-item columms="4">
         <x-layout.modal.modal-add-body-group-item-label item="producebrand_id" title="MARCA" plus="producebrand"/>
 
         <select wire:model="producebrand_id" class="form-select form-select-sm text-uppercase" id="producebrand_id">
@@ -25,7 +39,7 @@
         <x-layout.modal.modal-add-body-group-item-error item="producebrand_id" message="$message"/>
     </x-layout.modal.modal-add-body-group-item>
 
-    <x-layout.modal.modal-add-body-group-item columms="6">
+    <x-layout.modal.modal-add-body-group-item columms="4">
         <x-layout.modal.modal-add-body-group-item-label item="producemeasure_id" title="EMBALAGEM" plus="producemeasure"/>
 
         <select wire:model="producemeasure_id" class="form-select form-select-sm text-uppercase" id="producemeasure_id">
