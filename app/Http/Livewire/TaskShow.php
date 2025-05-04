c854e0a8270d9706136384ff50e517ce1c06cc19
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2025-05-04 12:40:31 -0300

    sis

--- app/Http/Livewire/TaskShow.php
+++ app/Http/Livewire/TaskShow.php
@@ -2,12 +2,306 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Report;
+
+use App\Models\Page;
+use App\Models\Task;
+
+use Livewire\WithPagination;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class TaskShow extends Component
 {
-    public function render()
-    {
-        return view('livewire.task-show');
+    use WithFileUploads;
+    use WithPagination;
+
+    protected $paginationTheme = 'bootstrap';
+    protected $listeners = ['refreshChildren' => 'refreshMe'];
+
+    public function refreshMe(){}
+
+    public $config;
+
+    public $search = '';
+    public $filter = 'name';
+
+    public $report_id;
+    public $mail;
+    public $comment;
+
+    public $task_id;
+    public $name;
+    public $status;
+    public $created;
+
+    /**
+     * Construtor.
+     */
+    public function mount($config){
+        $this->config = $config;
     }
+
+    /**
+     * Valida campos gerais.
+     */
+    protected function rules()
+    {
+        return [
+            'report_id' => ['required'],
+            'mail'      => ['required', 'email', 'between:2,255'],
+            'comment'   => ['nullable', 'between:2,255'],
+
+            'name' => ['required', 'between:2,255', 'unique:tasks,name,'.$this->task_id.''],
+        ];
+    }
+
+    /**
+     * Valida atualização.
+     */
+    public function updated($fields){
+        $this->validateOnly($fields);
+    }
+
+    /**
+     * Fecha Modals.
+     */
+    public function closeModal(){
+        $this->resetInput();
+        $this->resetValidation();
+    }
+
+    /**
+     * Reseta atributos.
+     */
+    public function resetInput()
+    {
+        $this->report_id = '';
+        $this->mail      = '';
+        $this->comment   = '';
+
+        $this->task_id = '';
+        $this->name    = '';
+        $this->status  = '';
+        $this->created = '';
+    }
+
+    /**
+     * Atualiza conteúdo sem atualizar página.
+     */
+    public function refresh()
+    {
+        $this->emit('refreshChildren');
+    }
+
+    /**
+     * Renderiza página.
+     */
+    public function render(){
+        return view('livewire.' . $this->config['name'] . '-show', [
+            'config'       => $this->config,
+            'existsItem'   => Task::exists(),
+            'existsReport' => Report::where('folder', $this->config['name'])->exists(),
+            'reports'      => Report::where('folder', $this->config['name'])->orderBy('id', 'DESC')->limit(12)->get(),
+            'list'         => Task::where([
+                                [$this->filter, 'like', '%'. $this->search . '%'],
+                            ])->orderBy('name', 'ASC')->paginate(12),
+        ]);
+    }
+
+    /**
+     * add()
+     *  register()
+     */
+    public function add()
+    {
+        //...
+    }
+        public function register()
+        {
+            // Valida campos.
+            $validatedData = $this->validate([
+                'name' => ['required', 'between:2,255', 'unique:tasks'],
+            ]);
+
+            // Define $data.
+            $data['config']        = $this->config;
+            $data['validatedData'] = $validatedData;
+
+            // Valida cadastro.
+            $valid = Task::validateAdd($data);
+
+            // Cadastra.
+            if ($valid) Task::add($data);
+
+            // Executa dependências.
+            if ($valid) Task::dependencyAdd($data);
+
+            // Fecha modal.
+            $this->closeModal();
+            $this->dispatchBrowserEvent('close-modal');
+        }
+
+    /** 
+     * detail()
+     */
+    public function detail(int $task_id)
+    {
+        // Tarefa.
+        $task = Task::find($task_id);
+
+        // Inicializa propriedades dinâmicas.
+        $this->task_id = $task->id;
+        $this->name    = $task->name;
+        $this->status  = $task->status;
+        $this->created = $task->created_at->format('d/m/Y H:i:s');
+    }
+
+    /**
+     * edit()
+     *  modernize()
+     */
+    public function edit(int $task_id)
+    {
+        // Tarefa.
+        $task = Task::find($task_id);
+
+        // Inicializa propriedades dinâmicas.
+        $this->task_id = $task->id;
+        $this->name    = $task->name;
+        $this->status  = $task->status;
+        $this->created = $task->created_at->format('d/m/Y H:i:s');
+    }
+        public function modernize()
+        {
+            // Valida campos.
+            $validatedData = $this->validate([
+                'name' => ['required', 'between:2,255', 'unique:tasks,name,'.$this->task_id.''],
+            ]);
+
+            // Estende $validatedData
+            $validatedData['task_id'] = $this->task_id;
+            $this->status ? $validatedData['status'] = true : $validatedData['status'] = false;
+
+            // Define $data.
+            $data['config']        = $this->config;
+            $data['validatedData'] = $validatedData;
+
+            // Valida atualização.
+            $valid = Task::validateEdit($data);
+
+            // Atualiza.
+            if ($valid) Task::edit($data);
+
+            // Executa dependências.
+            if ($valid) Task::dependencyEdit($data);
+
+            // Fecha modal.
+            $this->closeModal();
+            $this->dispatchBrowserEvent('close-modal');
+        }
+
+    /**
+     * erase()
+     *  exclude()
+     */
+    public function erase(int $task_id)
+    {
+        // Tarefa.
+        $task = Task::find($task_id);
+
+        // Inicializa propriedades dinâmicas.
+        $this->task_id = $task->id;
+        $this->name    = $task->name;
+        $this->status  = $task->status;
+        $this->created = $task->created_at->format('d/m/Y H:i:s');
+    }
+        public function exclude()
+        {
+            // Define $validatedData
+            $validatedData['task_id'] = $this->task_id;
+            $validatedData['name']    = $this->name;
+            $validatedData['status']  = $this->status;
+
+            // Define $data.
+            $data['config']        = $this->config;
+            $data['validatedData'] = $validatedData;
+
+            // Valida exclusão.
+            $valid = Task::validateErase($data);
+
+            // Executa dependências.
+            if ($valid) Task::dependencyErase($data);
+
+            // Exclui.
+            if ($valid) Task::erase($data);
+
+            // Fecha modal.
+            $this->closeModal();
+            $this->dispatchBrowserEvent('close-modal');
+        }
+
+    /**
+     * generate()
+     *  sire()
+     */
+    public function generate()
+    {
+        //...
+    }
+        public function sire()
+        {
+            // Define $data.
+            $data['config'] = $this->config;
+            $data['filter'] = $this->filter;
+            $data['search'] = $this->search;
+
+            // Valida geração de relatório.
+            $valid = Task::validateGenerate($data);
+
+            // Gera relatório.
+            if ($valid) Task::generate($data);
+
+            // Executa dependências.
+            if ($valid) Task::dependencyGenerate($data);
+
+            // Fecha modal.
+            $this->closeModal();
+            $this->dispatchBrowserEvent('close-modal');
+        }
+
+    /**
+     * mail()
+     *  send()
+     */
+    public function mail()
+    {
+        //...
+    }
+        public function send()
+        {
+            // Valida campos.
+            $validatedData = $this->validate([
+                'report_id' => ['required'],
+                'mail'      => ['required', 'email', 'between:2,255'],
+                'comment'   => ['nullable', 'between:2,255'],
+            ]);
+
+            // Define $data
+            $data['config']        = $this->config;
+            $data['validatedData'] = $validatedData;
+
+            // Valida envio do e-mail.
+            $valid = Task::validateMail($data);
+
+            // Envia e-mail.
+            if ($valid) Task::mail($data);
+
+            // Executa dependências.
+            if ($valid) Task::dependencyMail($data);
+
+            // Fecha modal.
+            $this->closeModal();
+            $this->dispatchBrowserEvent('close-modal');
+        }
 }
